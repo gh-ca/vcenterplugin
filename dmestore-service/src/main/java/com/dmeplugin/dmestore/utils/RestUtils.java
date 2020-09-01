abd6e9eefb60f5bbfe12aaaec00effdb4f683ad4
andrew.liu <andrew.liu@gh-ca.com>
2020-09-01 12:00:41 +0800

    修正resttemplate ssl 过滤方法

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/utils/RestUtils.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/utils/RestUtils.java
@@ -2,37 +2,41 @@ package com.dmeplugin.dmestore.utils;
 
 
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
+
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by yuanqi on 2020/8/31.
  */
 public class RestUtils {
-    public static void main( String[] args ){
-//        SSLContext sslContext = new SSLContextBuilder().
-//                loadTrustMaterial(TrustSelfSignedStrategy.INSTANCE).
-//                build();
-//
-//        HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
-//
-//        // X509HostnameVerifier hostnameVerifier = SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
-//        SSLConnectionSocketFactory systemSocketFactory = new SSLConnectionSocketFactory(
-//                sslContext,
-//                hostnameVerifier);
-//        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(systemSocketFactory).setMaxConnTotal(200).setMaxConnPerRoute(20).build();
-//
-//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-//
-//        requestFactory.setHttpClient(httpClient);
-//        requestFactory.setReadTimeout(185000);
+    public static void main( String[] args ) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        SSLContextBuilder builder = new SSLContextBuilder();
+        builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+
+        //HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+
+         X509HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+        SSLConnectionSocketFactory systemSocketFactory = new SSLConnectionSocketFactory(
+                builder.build(),
+                hostnameVerifier);
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(systemSocketFactory).setMaxConnTotal(200).setMaxConnPerRoute(20).build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        requestFactory.setReadTimeout(185000);
         RestTemplate template = new RestTemplate();
     }
 }
