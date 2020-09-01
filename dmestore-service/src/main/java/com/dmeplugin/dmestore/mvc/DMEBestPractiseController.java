917c547eddc6ae73f790ef75e90b9021f1f0f3c8
andrew.liu <andrew.liu@gh-ca.com>
2020-09-01 16:59:50 +0800

    controller类更新

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/DMEBestPractiseController.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/DMEBestPractiseController.java
@@ -4,6 +4,7 @@ import com.dmeplugin.dmestore.model.ResponseBodyBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,6 +16,28 @@ public class DMEBestPractiseController extends BaseController{
     @RequestMapping(value = "/manualupdate", method = RequestMethod.POST)
     @ResponseBody
     public ResponseBodyBean manualupdate()
+            throws Exception {
+
+        return success();
+    }
+
+    /**
+     * second entry for apply best practise on host
+     */
+    @RequestMapping(value = "/manualapplyforhost", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean applyforhost(@RequestParam(value = "hostid", required = true) String hostid)
+            throws Exception {
+
+        return success();
+    }
+
+    /**
+     * second entry for apply best practise on cluster
+     */
+    @RequestMapping(value = "/manualapplyforcluster", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean applyforcluster(@RequestParam(value = "clusterid", required = true) String clusterid)
             throws Exception {
 
         return success();
