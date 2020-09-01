c4daa54c26bffcda7d911fb3722ce75350274141
andrew.liu <andrew.liu@gh-ca.com>
2020-09-01 16:46:10 +0800

    controller类更新

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/DMEBestPractiseController.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/DMEBestPractiseController.java
@@ -1,19 +1,16 @@
 package com.dmeplugin.dmestore.mvc;
 
 import com.dmeplugin.dmestore.model.ResponseBodyBean;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/servicelevel")
-public class DMEServiceLevelController extends BaseController{
+@RequestMapping(value = "/bestpractise")
+public class DMEBestPractiseController extends BaseController{
     /**
-     * manual update service level
+     * manual update best practise
      */
     @RequestMapping(value = "/manualupdate", method = RequestMethod.POST)
     @ResponseBody
