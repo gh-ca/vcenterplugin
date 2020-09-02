fbc96b5b51d12308e5c25eee93e91e1d4ccafab0
liuxh <xiaohuan.liu@gh-ca.com>
2020-09-02 18:44:19 +0800

    获取服务等级列表接口，从api返回的json中解析出前台数据bean

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/services/ServiceLevelServiceImpl.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/services/ServiceLevelServiceImpl.java
@@ -2,7 +2,7 @@ package com.dmeplugin.dmestore.services;
 
 import com.dmeplugin.dmestore.model.ServiceLevelInfo;
 import com.dmeplugin.dmestore.utils.RestUtils;
-import com.google.gson.Gson;
+import com.google.gson.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,7 +46,8 @@ public class ServiceLevelServiceImpl implements ServiceLevelService {
                 remap.put("message", "list serviceLevel error!");
                 return remap;
             }
-            slis = convertBean(remap);
+            Object object = responseEntity.getBody();
+            slis = convertBean(object);
             remap.put("data", slis);
         } catch (Exception e) {
             log.error("list serviceLevel error", e);
@@ -84,6 +85,45 @@ public class ServiceLevelServiceImpl implements ServiceLevelService {
     private List<ServiceLevelInfo> convertBean(Object object) {
         List<ServiceLevelInfo> slis = new ArrayList<>();
 
+        JsonObject jsonObject = new JsonParser().parse(object.toString()).getAsJsonObject();
+        JsonArray jsonArray = jsonObject.get("service-levels").getAsJsonArray();
+        for(JsonElement jsonElement : jsonArray){
+            try {
+                JsonObject object1 = new JsonParser().parse(jsonElement.toString()).getAsJsonObject();
+                String id = object1.get("id").getAsString();
+                String name = object1.get("name").getAsString();
+                String type = object1.get("type").getAsString();
+                String protocol = object1.get("protocol").getAsString();
+                double totalCapacity = object1.get("total_capacity").getAsDouble();
+                double freeCapacity = object1.get("free_capacity").getAsDouble();
+
+                JsonObject qosParamObject = object1.get("capabilities").getAsJsonObject().get("qos").getAsJsonObject().get("qos_param").getAsJsonObject();
+                int minIOPS = qosParamObject.get("minIOPS").getAsInt();
+                int latency = qosParamObject.get("latency").getAsInt();
+                String latencyUnit = qosParamObject.get("latencyUnit").getAsString();
+
+                ServiceLevelInfo sli = new ServiceLevelInfo();
+                sli.setId(id);
+                sli.setName(name);
+                sli.setType(type);
+                sli.setProtocol(protocol);
+                sli.setTotalCapacity(totalCapacity);
+                sli.setFreeCapacity(freeCapacity);
+                sli.setMinIOPS(minIOPS);
+                sli.setLatency(latency);
+                sli.setLatencyUnit(latencyUnit);
+
+                slis.add(sli);
+            } catch (Exception e){
+                log.warn("servicelevel convert error:",e);
+            }
+        }
         return slis;
     }
+
+    /*public static void main(String[] args) {
+        String str = "{\"service-levels\":[{\"id\": \"UUID\",\"name\": \"service-level_block\",\"description\": \"block service-level for dj\",\"type\":\"BLOCK\",\"protocol\": \"FC\",\"total_capacity\": 200,\"free_capacity\": 200,\"used_capacity\":100,\"capabilities\": {\"resource_type\": \"thin\",\"compression\": true,\"deduplication\": true,\"smarttier\": {\"policy\": \"1\",\"enabled\": true},\"qos\": {\"enabled\": true,\"qos_param\":{\"latency\":\"10\",\"latencyUnit\": \"ms\",\"minBandWidth\": 1000,\"minIOPS\": 1000}}}}]}";
+        ServiceLevelServiceImpl sls = new ServiceLevelServiceImpl();
+        sls.convertBean(str);
+    }*/
 }
