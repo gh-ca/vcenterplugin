52b8c2f0734d31b2b9c963c6e6701b80859b1323
yuanyu <yu.yuan@gh-ca.com>
2020-09-02 22:16:10 +0800

    vmfs列表

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/model/VmfsDataInfo.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/model/VmfsDataInfo.java
@@ -4,9 +4,11 @@ public class VmfsDataInfo {
     //列表字段（基本视图）
     String name;    //名称
     String status;  //状态
+
     Double capacity;  //总容量 单位GB
     Double freeSpace; //空闲容量 单位GB
     Double reserveCapacity; //置备容量  capacity+uncommitted-freeSpace 单位GB
+
     String device; //设备
     String serviceLevelName;    //服务等级
     Boolean vmfsProtected;  //保护状态
@@ -19,4 +21,107 @@ public class VmfsDataInfo {
 
     int latency; //时延 单位ms
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Double capacity) {
+        this.capacity = capacity;
+    }
+
+    public Double getFreeSpace() {
+        return freeSpace;
+    }
+
+    public void setFreeSpace(Double freeSpace) {
+        this.freeSpace = freeSpace;
+    }
+
+    public Double getReserveCapacity() {
+        return reserveCapacity;
+    }
+
+    public void setReserveCapacity(Double reserveCapacity) {
+        this.reserveCapacity = reserveCapacity;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getServiceLevelName() {
+        return serviceLevelName;
+    }
+
+    public void setServiceLevelName(String serviceLevelName) {
+        this.serviceLevelName = serviceLevelName;
+    }
+
+    public Boolean getVmfsProtected() {
+        return vmfsProtected;
+    }
+
+    public void setVmfsProtected(Boolean vmfsProtected) {
+        this.vmfsProtected = vmfsProtected;
+    }
+
+    public int getMaxIops() {
+        return maxIops;
+    }
+
+    public void setMaxIops(int maxIops) {
+        this.maxIops = maxIops;
+    }
+
+    public int getMinIops() {
+        return minIops;
+    }
+
+    public void setMinIops(int minIops) {
+        this.minIops = minIops;
+    }
+
+    public int getMaxBandwidth() {
+        return maxBandwidth;
+    }
+
+    public void setMaxBandwidth(int maxBandwidth) {
+        this.maxBandwidth = maxBandwidth;
+    }
+
+    public int getMinBandwidth() {
+        return minBandwidth;
+    }
+
+    public void setMinBandwidth(int minBandwidth) {
+        this.minBandwidth = minBandwidth;
+    }
+
+    public int getLatency() {
+        return latency;
+    }
+
+    public void setLatency(int latency) {
+        this.latency = latency;
+    }
 }
--- dmestore-service/src/main/java/com/dmeplugin/dmestore/services/VmfsAccessServiceImpl.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/services/VmfsAccessServiceImpl.java
@@ -17,10 +17,7 @@ import org.springframework.http.*;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class VmfsAccessServiceImpl implements VmfsAccessService {
 
@@ -35,25 +32,65 @@ public class VmfsAccessServiceImpl implements VmfsAccessService {
 
     @Override
     public List<VmfsDataInfo> listVmfs() throws Exception {
+        List<VmfsDataInfo> relists = null;
         try{
             //取得vcenter中的所有vmfs存储。
             String listStr = VCSDKUtils.getAllVmfsDataStores();
             if(!StringUtils.isEmpty(listStr)) {
                 JsonArray jsonArray = new JsonParser().parse(listStr).getAsJsonArray();
                 if(jsonArray!=null && jsonArray.size()>0) {
+                    relists = new ArrayList<>();
                     for(int i=0;i<jsonArray.size();i++) {
                         JsonObject jo = jsonArray.get(i).getAsJsonObject();
                         LOG.info("jo=="+jo.toString());
-                        //然后通过vmfs中的url值去DME系统中查询对应的卷信息。
+
+                        VmfsDataInfo vmfsDataInfo = new VmfsDataInfo();
+                        double capacity = getDouble(jo.get("capacity").getAsString())/1024/1024/1024;
+                        double freeSpace = getDouble(jo.get("freeSpace").getAsString())/1024/1024/1024;
+                        double uncommitted = getDouble(jo.get("uncommitted").getAsString())/1024/1024/1024;
+
+                        vmfsDataInfo.setName(jo.get("name").getAsString());
+
+                        vmfsDataInfo.setCapacity(capacity);
+                        vmfsDataInfo.setFreeSpace(freeSpace);
+                        vmfsDataInfo.setReserveCapacity(capacity+uncommitted-freeSpace);
+
+                        String wwn = jo.get("url").getAsString();
+                        LOG.info("wwn=="+wwn);
+                        //然后通过vmfs中的url值去DME系统中查询对应wwn的卷信息。
+                        ///rest/blockservice/v1/volumes?volume_wwn=wwn
                         //这里由于DME系统中的卷太多。是分页查询，所以需要vmfs一个个的去查DME系统中的卷。
                         //而每次查询DME中的卷都需要调用两次，分别是查卷列表接口，查卷详细接口。
-//                        ResponseEntity responseEntity = dmeAccessService.access(LIST_VOLUME_URL, HttpMethod.GET, null);
-//                        LOG.info("listVmfs responseEntity==" + responseEntity.toString());
-//                        if (responseEntity.getStatusCodeValue() == 200) {
-//                            JsonObject jsonObject = new JsonParser().parse(responseEntity.getBody().toString()).getAsJsonObject();
-//                            LOG.info("listVmfs jsonObject==" + jsonObject.toString());
-//
-//                        }
+                        ResponseEntity responseEntity = dmeAccessService.access(LIST_VOLUME_URL+"?volume_wwn="+wwn, HttpMethod.GET, null);
+                        LOG.info("listVmfs responseEntity==" + responseEntity.toString());
+                        if (responseEntity.getStatusCodeValue() == 200) {
+                            JsonObject jsonObject = new JsonParser().parse(responseEntity.getBody().toString()).getAsJsonObject();
+                            LOG.info("listVmfs jsonObject==" + jsonObject.toString());
+                            JsonObject vjson = jsonObject.getAsJsonArray("volumes").get(0).getAsJsonObject();
+
+                            vmfsDataInfo.setStatus(vjson.get("status").getAsString());
+                            vmfsDataInfo.setServiceLevelName(vjson.get("service_level_name").getAsString());
+                            vmfsDataInfo.setVmfsProtected(vjson.get("protected").getAsBoolean());
+
+                            String volid = vjson.get("id").getAsString();
+                            //通过卷ID再调卷详细接口
+                            responseEntity = dmeAccessService.access(LIST_VOLUME_URL+"/"+volid, HttpMethod.GET, null);
+                            LOG.info("volid responseEntity==" + responseEntity.toString());
+                            if (responseEntity.getStatusCodeValue() == 200) {
+                                JsonObject voljson = new JsonParser().parse(responseEntity.getBody().toString()).getAsJsonObject();
+                                LOG.info("volid voljson==" + voljson.toString());
+                                JsonObject vjson2 = voljson.getAsJsonObject("volume");
+
+
+                                //通过卷ID再调卷详细接口
+
+
+                            }
+
+
+                        }
+                        relists.add(vmfsDataInfo);
+
                     }
                 }
             }
@@ -62,7 +99,8 @@ public class VmfsAccessServiceImpl implements VmfsAccessService {
             LOG.error("list vmfs error:"+e);
             throw e;
         }
-        return null;
+        LOG.info("relists==="+(relists==null?"null":(relists.size()+"=="+gson.toJson(relists))));
+        return relists;
     }
 
     @Override
@@ -73,5 +111,17 @@ public class VmfsAccessServiceImpl implements VmfsAccessService {
     @Override
     public void mountVmfs(Map<String, Object> params) throws Exception {
 
+    }
+
+    private double getDouble(String obj){
+        double re = 0;
+        try {
+            if(!StringUtils.isEmpty(obj)){
+                re = Double.parseDouble(obj);
+            }
+        }catch (Exception e){
+            LOG.error("error:",e);
+        }
+        return re;
     }
 }
