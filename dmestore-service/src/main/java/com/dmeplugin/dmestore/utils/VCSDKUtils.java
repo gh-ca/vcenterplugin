81334f06b99b904e28b334b3d32009af69dcb89c
yuanyu <yu.yuan@gh-ca.com>
2020-09-04 09:45:08 +0800

    修改vcenter的获取

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/utils/VCSDKUtils.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/utils/VCSDKUtils.java
@@ -4,7 +4,9 @@ import com.dmeplugin.dmestore.model.DatastoreInfo;
 import com.dmeplugin.dmestore.services.EchoServiceImpl;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.vmware.vim25.DatastoreSummary;
+import com.vmware.connection.BasicConnection;
+import com.vmware.connection.Connection;
+import com.vmware.vim25.*;
 import com.vmware.vim25.mo.Datastore;
 import com.vmware.vim25.mo.InventoryNavigator;
 import com.vmware.vim25.mo.ManagedEntity;
@@ -25,19 +27,22 @@ public class VCSDKUtils {
 
     public static String getAllVmfsDataStores(){
         String listStr = "";
+        ConnectedVimServiceBase connectedVimServiceBase = null;
         try {
-            _logger.info("startme");
-            URL serverUrl = new URL("https://10.143.132.248/sdk");
-            ServiceInstance serviceInstance = new ServiceInstance(serverUrl, "administrator@vsphere.local", "Pbu4@123", true);
+            connectedVimServiceBase=new ConnectedVimServiceBase();
+            Connection connection=new BasicConnection();
+            connection.setUrl("https://10.143.132.248:443/sdk");
+            connection.setUsername("administrator@vsphere.local");
+            connection.setPassword("Pbu4@123");
+            connectedVimServiceBase.setConnection(connection);
+            connectedVimServiceBase.connect();
 
-            ManagedEntity[] objs = new InventoryNavigator(serviceInstance.getRootFolder()).searchManagedEntities("Datastore");
-            _logger.info("after");
-            if(objs!=null && objs.length>0) {
+            DatastoreVimBase datastoreVimBase=new DatastoreVimBase(connectedVimServiceBase);
+
+            List<DatastoreSummary> datastoreSummaryList=datastoreVimBase.getAllDatastoreSummary();
+            if(datastoreSummaryList!=null && datastoreSummaryList.size()>0) {
                 List<DatastoreSummary> lists = new ArrayList<>();
-                for (ManagedEntity managedEntity : objs) {
-                    _logger.info(managedEntity.getName());
-                    Datastore da = (Datastore) managedEntity;
-                    DatastoreSummary datastoreSummary = da.getSummary();
+                for (DatastoreSummary datastoreSummary : datastoreSummaryList) {
                     if(datastoreSummary.getType().equals(STORE_TYPE_VMFS)){
                         lists.add(datastoreSummary);
                     }
@@ -49,7 +54,50 @@ public class VCSDKUtils {
             }
         }catch (Exception e){
             e.printStackTrace();
+        }finally {
+            if(connectedVimServiceBase!=null) {
+                connectedVimServiceBase.disconnect();
+            }
         }
         return listStr;
+    }
+
+    public static void main(String[] args) throws InvalidPropertyFaultMsg, RuntimeFaultFaultMsg {
+        ConnectedVimServiceBase connectedVimServiceBase=new ConnectedVimServiceBase();
+        Connection connection=new BasicConnection();
+        connection.setUrl("https://10.143.132.248:443/sdk");
+        connection.setUsername("administrator@vsphere.local");
+        connection.setPassword("Pbu4@123");
+        connectedVimServiceBase.setConnection(connection);
+        connectedVimServiceBase.connect();
+        /*Map<String, ManagedObjectReference> mapme= datastoreVimBase.getDatastores();
+        for (String key:mapme.keySet()){
+            System.out.println(key+mapme.get(key));
+        }*/
+
+        DatastoreVimBase datastoreVimBase=new DatastoreVimBase(connectedVimServiceBase);
+
+
+//        List<DatastoreInfo> dslist=datastoreVimBase.getAllDatastoreInfo();
+//
+//        for (DatastoreInfo ds:dslist){
+//            System.out.println(ds.getName());
+//        }
+
+        List<DatastoreSummary> datastoreSummaryList=datastoreVimBase.getAllDatastoreSummary();
+
+        for (DatastoreSummary ds:datastoreSummaryList){
+            System.out.println(ds.getName()+"=="+ds.getType()+"=="+ds.getCapacity());
+        }
+
+//        HostSystemVimBase hostSystemVimBase=new HostSystemVimBase(connectedVimServiceBase);
+//        List<HostListSummary> hostsummarylist=hostSystemVimBase.getAllHostSummary();
+//
+//        ClusterVimBase clusterVimBase=new ClusterVimBase(connectedVimServiceBase);
+//        List<ComputeResourceSummary> clustersummarylist=clusterVimBase.getAllClusterSummary();
+//
+//        ComputeResourceSummary clustersummary=clusterVimBase.getClusterSummaryByName("pbu4test");
+
+        connectedVimServiceBase.disconnect();
     }
 }
