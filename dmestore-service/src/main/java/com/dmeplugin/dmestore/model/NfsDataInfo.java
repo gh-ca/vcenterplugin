66d6c00396b2e3c5bcbf918017338465aa5a99d4
yuanyu <yu.yuan@gh-ca.com>
2020-09-07 11:48:03 +0800

    nfs列表

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/model/NfsDataInfo.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/model/NfsDataInfo.java
@@ -14,9 +14,121 @@ public class NfsDataInfo {
     String fs; //fs
 
     //列表字段（性能视图）：
-    int OPS; //OPS
-    int bandwidth;   //带宽 单位MB/s
+    Double OPS; //OPS
+    Double bandwidth;   //带宽 单位MB/s
 
     int readResponseTime;   //读响应时间 单位ms
     int writeResponseTime; //写响应时间 单位ms
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Double capacity) {
+        this.capacity = capacity;
+    }
+
+    public Double getFreeSpace() {
+        return freeSpace;
+    }
+
+    public void setFreeSpace(Double freeSpace) {
+        this.freeSpace = freeSpace;
+    }
+
+    public Double getReserveCapacity() {
+        return reserveCapacity;
+    }
+
+    public void setReserveCapacity(Double reserveCapacity) {
+        this.reserveCapacity = reserveCapacity;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getLogicPort() {
+        return logicPort;
+    }
+
+    public void setLogicPort(String logicPort) {
+        this.logicPort = logicPort;
+    }
+
+    public String getShareIp() {
+        return shareIp;
+    }
+
+    public void setShareIp(String shareIp) {
+        this.shareIp = shareIp;
+    }
+
+    public String getSharePath() {
+        return sharePath;
+    }
+
+    public void setSharePath(String sharePath) {
+        this.sharePath = sharePath;
+    }
+
+    public String getFs() {
+        return fs;
+    }
+
+    public void setFs(String fs) {
+        this.fs = fs;
+    }
+
+    public Double getOPS() {
+        return OPS;
+    }
+
+    public void setOPS(Double OPS) {
+        this.OPS = OPS;
+    }
+
+    public Double getBandwidth() {
+        return bandwidth;
+    }
+
+    public void setBandwidth(Double bandwidth) {
+        this.bandwidth = bandwidth;
+    }
+
+    public int getReadResponseTime() {
+        return readResponseTime;
+    }
+
+    public void setReadResponseTime(int readResponseTime) {
+        this.readResponseTime = readResponseTime;
+    }
+
+    public int getWriteResponseTime() {
+        return writeResponseTime;
+    }
+
+    public void setWriteResponseTime(int writeResponseTime) {
+        this.writeResponseTime = writeResponseTime;
+    }
 }
