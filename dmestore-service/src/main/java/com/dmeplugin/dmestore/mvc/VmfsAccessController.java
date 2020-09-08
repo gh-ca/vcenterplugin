4b9b669fbc9881b24c92dfdc484bea130ab852fc
yuanyu <yu.yuan@gh-ca.com>
2020-09-08 09:26:03 +0800

    增加主机列表

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/VmfsAccessController.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/VmfsAccessController.java
@@ -74,7 +74,8 @@ public class VmfsAccessController extends BaseController{
     /*
    * Create vmfs
    * ServiceVolumeBasicParams对象包含如下属性
-   * param str name: 名称 必
+   * param str name: 名称 必 vmfs stor的名称
+   * param str volumeName: 名称 必 卷的名称
    * param int capacity: 容量，单位GB 必
    * param int count: 数量 必
    * param int start_suffix: 该规格卷的起始后缀编号
@@ -85,7 +86,7 @@ public class VmfsAccessController extends BaseController{
    * param str host_id: 主机id，与hostgroup_id二选其一,不可同时存在
    * param str hostgroup_id: 主机组id，与host_id二选其一，不可同时存在
 
-   * param str service_level_id: 服务等级id 必
+   * param str service_level_id: 服务等级id 若未选择服务等级，可选择存储设备、存储池、设置QoS、Thin、Workload
    * param str project_id: 业务群组id
    * param str availability_zone: 可用分区id
    *
@@ -95,6 +96,19 @@ public class VmfsAccessController extends BaseController{
    * param str spaceReclamationPriority: 空间回收优先权
    * param str host: 主机
    * param str cluster: 集群
+   *
+   * param str storage_id 存储设备id
+   * param str pool_raw_id 卷所属存储池在存储设备上的id
+   * param str workload_type_id 应用类型id
+   *
+   * 卷qos属性
+   * param integer  control_policy 控制策略
+   * param integer  latency 时延，单位ms
+   * param integer  maxbandwidth 最大带宽
+   * param integer  maxiops 最大iops
+   * param integer  minbandwidth 最小带宽
+   * param integer  miniops 最小iops
+   * param str name Smart QoS名称
    * return: Return execution status and information
    *         code:Status code 200 or 503
    *         message:Information
