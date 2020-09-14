94104a07fbf4975510e8edcf6b59b978de19da33
wangxiangyong <111@163.com>
2020-09-14 14:19:19 +0800

    虚拟机RDM创建优化、整理

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/VMRDMController.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/VMRDMController.java
@@ -2,28 +2,11 @@ package com.dmeplugin.dmestore.mvc;
 
 import com.dmeplugin.dmestore.model.ResponseBodyBean;
 import com.dmeplugin.dmestore.model.VmRDMCreateBean;
-import com.dmeplugin.dmestore.services.DmeAccessService;
-import com.dmeplugin.dmestore.services.TaskService;
 import com.dmeplugin.dmestore.services.VMRDMService;
-import com.dmeplugin.dmestore.utils.VCSDKUtils;
-import com.dmeplugin.vmware.mo.DatacenterMO;
-import com.dmeplugin.vmware.mo.DatastoreMO;
-import com.dmeplugin.vmware.mo.VirtualMachineMO;
-import com.dmeplugin.vmware.util.TestVmwareContextFactory;
-import com.dmeplugin.vmware.util.VmwareContext;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.vmware.vim25.VirtualDiskMode;
-import com.vmware.vim25.VirtualDiskType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "v1/vmrdm")
@@ -33,95 +16,18 @@ public class VMRDMController extends BaseController {
     @Autowired
     private VMRDMService vmrdmService;
 
-    @Autowired
-    private TaskService taskService;
-
-    @Autowired
-    private DmeAccessService dmeAccessService;
-
-    @Autowired
-    private Gson gson;
-
     @RequestMapping(value = "createRdm", method = RequestMethod.POST)
     public ResponseBodyBean createRDM(@RequestParam("datacenter_name") String datacenter_name,
                                       @RequestParam("datastore_name") String datastore_name,
                                       @RequestParam("vm_name") String vm_name,
                                       @RequestParam("host_id") String host_id,
                                       @RequestBody VmRDMCreateBean createBean) throws Exception {
-        String taskId = vmrdmService.createRDM(createBean);
-        if (null == taskId) {
-            LOG.error("Failed to create RDM on DME! taskId is null");
-            return failure("Failed to create RDM on DME!");
-        }
-        JsonObject taskDetail = taskService.queryTaskByIdUntilFinish(taskId);
-        if (null != taskDetail && taskDetail.get("status").getAsInt() != 3) {
-            LOG.error("Failed to create RDM on DME!taskDetail={}", gson.toJson(taskDetail));
-            return failure("Failed to create RDM on DME");
-        }
-        List<String> volumeIds = new ArrayList();
-        //获取卷资源
-        JsonArray resources = taskDetail.getAsJsonArray("resources");
-        for (int i = 0; i < resources.size(); i++) {
-            JsonObject resource = resources.get(i).getAsJsonObject();
-            String id = resource.get("id").getAsString();
-            volumeIds.add(id);
-        }
-        //将卷映射给主机
-        taskId = vmrdmService.hostMapping(host_id, volumeIds);
-        if (null == taskId) {
-            LOG.error("Disk mapping to host failed! taskId is null");
-            return failure("Disk mapping to host failed!");
-        }
-        taskDetail = taskService.queryTaskByIdUntilFinish(taskId);
-        if (null != taskDetail && taskDetail.get("status").getAsInt() == 3) {
-            LOG.info("Disk mapping to host succeeded!");
-        } else {
-            LOG.error("Disk mapping to host failed!taskDetail={}", gson.toJson(taskDetail));
-            return failure("Disk mapping to host failed!");
-        }
-
-        //查询主机信息
-        Map<String, Object> hostMap = dmeAccessService.getDmeHost(host_id);
-        String host_ip = hostMap.get("ip").toString();
-        String host_name = hostMap.get("name").toString();
-        //调用vcenter扫描卷
-        vmrdmService.hostRescanVmfs(host_ip);
-        LOG.info("RescanVmfs successful!");
-        //获取卷信息
-        String lunsStr = vmrdmService.getLunsOnHost(host_name);
-        JsonArray lunArray = gson.fromJson(lunsStr, JsonArray.class);
-        JsonObject lunObject = null;
-        boolean flag = false;
-        for (int i = 0; i < lunArray.size(); i++) {
-            lunObject = lunArray.get(i).getAsJsonObject();
-            LOG.info("Lun deviceName={}, requested name={}", lunObject.get("deviceName").getAsString(), createBean.getName());
-            //TODO 根据实际情况优化
-            if (lunObject.get("deviceName").getAsString().equals(createBean.getName())) {
-                flag = true;
-                break;
-            }
-        }
-
-        if (!flag) {
-            LOG.error("not find lun, disk mapping to host failed!");
-            return failure("Disk mapping to host failed!");
-        }
-        //调用Vcenter创建磁盘
-        VmwareContext vmwareContext = TestVmwareContextFactory.getContext("10.143.132.248", "administrator@vsphere.local", "Pbu4@123");
-        DatacenterMO dcMo = new DatacenterMO(vmwareContext, datacenter_name);
-        DatastoreMO dsMo = new DatastoreMO(vmwareContext, dcMo.findDatastore(datastore_name));
-        VirtualMachineMO virtualMachineMO = new VirtualMachineMO(vmwareContext, vm_name);
-        String rdmdevicename = lunObject.get("deviceName").getAsString();
-        String vmdkDatastorePath = dsMo.getDatastorePath(datastore_name);
-        int sizeInMb = createBean.getSize();
         try {
-            virtualMachineMO.createDisk(vmdkDatastorePath, VirtualDiskType.RDM, VirtualDiskMode.PERSISTENT,
-                    rdmdevicename, sizeInMb, dsMo.getMor(), -1);
-        }catch (Exception ex){
-            LOG.error("create vcenter disk rdm failed!errorMsg:{}", ex.getMessage());
-            return failure("Create Vcenter disk RDM failed");
+            vmrdmService.createRDM(datacenter_name, datastore_name, vm_name, host_id, createBean);
+            return success();
+        }catch (Exception e){
+            LOG.error(e.getMessage());
+            return failure(e.getMessage());
         }
-
-        return success();
     }
 }
--- dmestore-service/src/main/java/com/dmeplugin/dmestore/services/VMRDMServiceImpl.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/services/VMRDMServiceImpl.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -28,6 +30,9 @@ public class VMRDMServiceImpl implements VMRDMService {
     private DmeAccessService dmeAccessService;
 
     @Autowired
+    private TaskService taskService;
+
+    @Autowired
     private Gson gson = new Gson();
 
     private VCSDKUtils vcsdkUtils;
@@ -41,61 +46,95 @@ public class VMRDMServiceImpl implements VMRDMService {
     }
 
     @Override
-    public String createRDM(VmRDMCreateBean vmRDMCreateBean) {
+    public void createRDM(String datacenter_name, String datastore_name, String vm_name, String host_id, VmRDMCreateBean createBean) throws Exception {
+        List<String> volumeIds = createDmeRDM(createBean);
+        LOG.info("create DME disk succeeded!");
+        //将卷映射给主机
+        hostMapping(host_id, volumeIds);
+        LOG.info("disk mapping to host succeeded!");
+        //查询主机信息
+        Map<String, Object> hostMap = dmeAccessService.getDmeHost(host_id);
+        String host_ip = hostMap.get("ip").toString();
+        String host_name = hostMap.get("name").toString();
+        //调用vcenter扫描卷
+        vcsdkUtils.hostRescanVmfs(host_ip);
+        LOG.info("scan vmfs succeeded!");
+        //获取卷信息
+        String lunStr = vcsdkUtils.getLunsOnHost(host_name);
+        JsonArray lunArray = gson.fromJson(lunStr, JsonArray.class);
+        JsonObject lunObject = null;
+        boolean flag = false;
+        for (int i = 0; i < lunArray.size(); i++) {
+            lunObject = lunArray.get(i).getAsJsonObject();
+            LOG.info("Lun deviceName={}, requested name={}", lunObject.get("deviceName").getAsString(), createBean.getName());
+            //TODO 根据实际情况优化
+            if (lunObject.get("deviceName").getAsString().equals(createBean.getName())) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) {
+            LOG.error("no matching Lun information was found!");
+            throw new Exception("no matching Lun information was found!");
+        }
+        //调用Vcenter创建磁盘
+        vcsdkUtils.createDisk(datacenter_name, datastore_name, vm_name, lunObject.get("deviceName").getAsString(), createBean.getSize());
+    }
+
+    public List<String> createDmeRDM(VmRDMCreateBean vmRDMCreateBean) throws Exception {
         String url = DmeConstants.DME_VOLUME_BASE_URL;
         JsonObject body = new JsonObject();
         body.addProperty("service_level_id", vmRDMCreateBean.getServiceLevelId());
         JsonArray volumes = new JsonArray();
-        try {
-            JsonObject base = new JsonObject();
-            base.addProperty("name", vmRDMCreateBean.getName());
-            base.addProperty("capacity", vmRDMCreateBean.getSize());
-            base.addProperty("count", vmRDMCreateBean.getSize());
-            volumes.add(base);
-            body.add("volumes", volumes);
-            ResponseEntity<String> responseEntity = dmeAccessService.access(url, HttpMethod.POST, body.toString());
-            if (responseEntity.getStatusCodeValue() / 100 != 2) {
-                LOG.error("create dme rdm failed!errorMsg:{}", responseEntity.getBody());
-                return null;
-            }
-            JsonObject task = gson.fromJson(responseEntity.getBody(), JsonObject.class);
-            return task.get("task_id").getAsString();
-        } catch (Exception ex) {
-            LOG.error("create dme rdm error!errorMsg:{}", ex.getMessage());
+        JsonObject base = new JsonObject();
+        base.addProperty("name", vmRDMCreateBean.getName());
+        base.addProperty("capacity", vmRDMCreateBean.getSize());
+        base.addProperty("count", vmRDMCreateBean.getSize());
+        volumes.add(base);
+        body.add("volumes", volumes);
+        ResponseEntity<String> responseEntity = dmeAccessService.access(url, HttpMethod.POST, body.toString());
+        if (responseEntity.getStatusCodeValue() / 100 != 2) {
+            LOG.error("Failed to create RDM on DME!errorMsg:{}", responseEntity.getBody());
+            throw new Exception("Failed to create RDM on DME!");
         }
-        return null;
+        JsonObject task = gson.fromJson(responseEntity.getBody(), JsonObject.class);
+        String taskId = task.get("task_id").getAsString();
+        JsonObject taskDetail = taskService.queryTaskByIdUntilFinish(taskId);
+        if (null != taskDetail && taskDetail.get("status").getAsInt() != 3) {
+            LOG.error("The DME volume is in abnormal condition!taskDetail={}", gson.toJson(taskDetail));
+            throw new Exception("The DME volume is in abnormal condition!");
+        }
+        List<String> volumeIds = new ArrayList();
+        //获取卷资源
+        JsonArray resources = taskDetail.getAsJsonArray("resources");
+        for (int i = 0; i < resources.size(); i++) {
+            JsonObject resource = resources.get(i).getAsJsonObject();
+            String id = resource.get("id").getAsString();
+            volumeIds.add(id);
+        }
+        return volumeIds;
     }
 
-    @Override
-    public String hostMapping(String hostId, List<String> volumeIds) {
+    private void hostMapping(String hostId, List<String> volumeIds) throws Exception {
         String url = DmeConstants.DME_HOST_MAPPING_URL;
         JsonObject body = new JsonObject();
         body.addProperty("host_id", hostId);
         JsonArray volume_ids = gson.fromJson(gson.toJson(volumeIds), JsonArray.class);
         body.add("volume_ids", volume_ids);
-        try {
-            ResponseEntity<String> responseEntity = dmeAccessService.access(url, HttpMethod.POST, body.toString());
-            if (responseEntity.getStatusCodeValue() / 100 != 2) {
-                LOG.error("host mapping failed!errorMsg:{}", responseEntity.getBody());
-                return null;
-            }
-            JsonObject task = gson.fromJson(responseEntity.getBody(), JsonObject.class);
-            String taskId = task.get("task_id").getAsString();
-            return taskId;
-        } catch (Exception ex) {
-            LOG.error("host mapping error!errorMsg:{}", ex.getMessage());
+        ResponseEntity<String> responseEntity = dmeAccessService.access(url, HttpMethod.POST, body.toString());
+        if (responseEntity.getStatusCodeValue() / 100 != 2) {
+            LOG.error("host mapping failed!errorMsg:{}", responseEntity.getBody());
+            throw new Exception("");
         }
-        return null;
-    }
-
-    @Override
-    public void hostRescanVmfs(String hostIp) throws Exception {
-        vcsdkUtils.hostRescanVmfs(hostIp);
-    }
-
-    @Override
-    public String getLunsOnHost(String hostName) throws Exception {
-        return vcsdkUtils.getLunsOnHost(hostName);
+        JsonObject task = gson.fromJson(responseEntity.getBody(), JsonObject.class);
+        String taskId = task.get("task_id").getAsString();
+        JsonObject taskDetail = taskService.queryTaskByIdUntilFinish(taskId);
+        if (null != taskDetail && taskDetail.get("status").getAsInt() == 3) {
+            LOG.info("Disk mapping to host succeeded!");
+        } else {
+            LOG.error("Disk mapping to host failed!taskDetail={}", gson.toJson(taskDetail));
+        }
     }
 
 }
