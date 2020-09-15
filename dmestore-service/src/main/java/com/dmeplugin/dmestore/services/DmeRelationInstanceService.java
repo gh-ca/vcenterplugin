5b6252f22c497563e4a305c619534b8f4f876dd3
liuxh <xiaohuan.liu@gh-ca.com>
2020-09-15 18:15:15 +0800

    关系类型资源实例获取

---
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/services/DmeRelationInstanceService.java
@@ -0,0 +1,32 @@
+package com.dmeplugin.dmestore.services;
+
+import com.dmeplugin.dmestore.model.RelationInstance;
+
+import java.util.List;
+
+/**
+ * @Description: TODO
+ * @ClassName: DmeRelationInstanceService
+ * @Company: GH-CA
+ * @author: liuxh
+ * @create: 2020-09-15
+ **/
+public interface DmeRelationInstanceService {
+
+    /**
+     * 按资源关系类型名称查询资源关系实例
+     * @param relationName enum:M_DjTierContainsLun M_DjTierContainsStoragePool M_DjTierContainsStoragePort
+     * @return
+     * @throws Exception
+     */
+    List<RelationInstance> queryRelationByRelationName(String relationName) throws Exception;
+
+    /**
+     * 按资源关系类型名称和实例ID查询资源关系实例
+     * @param relationName enum:M_DjTierContainsLun M_DjTierContainsStoragePool M_DjTierContainsStoragePort
+     * @param instanceId uuid
+     * @return
+     * @throws Exception
+     */
+    RelationInstance queryRelationByRelationNameInstanceId(String relationName, String instanceId) throws Exception;
+}
