7fb34712e2b65f19a8c8d62793f6f8129d169cb3
liuxh <xiaohuan.liu@gh-ca.com>
2020-09-18 17:31:45 +0800

    性能数据获取接口修改 增加服务等级性能获取接口

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/services/DataStoreStatisticHistoryService.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/services/DataStoreStatisticHistoryService.java
@@ -10,14 +10,14 @@ import java.util.Map;
  * @create: 2020-09-03
  **/
 public interface DataStoreStatisticHistoryService {
-    String RANG_LAST_5_MINUTE = "LAST_5_MINUTE";
-    String RANG_LAST_1_HOUR = "LAST_1_HOUR";
-    String RANG_LAST_1_DAY = "LAST_1_DAY";
-    String RANG_LAST_1_WEEK = "LAST_1_WEEK";
-    String RANG_LAST_1_MONTH = "LAST_1_MONTH";
-    String RANG_LAST_1_QUARTER = "LAST_1_QUARTER";
-    String RANG_HALF_1_YEAR = "HALF_1_YEAR";
-    String RANG_LAST_1_YEAR = "LAST_1_YEAR";
+    String RANGE_LAST_5_MINUTE = "LAST_5_MINUTE";
+    String RANGE_LAST_1_HOUR = "LAST_1_HOUR";
+    String RANGE_LAST_1_DAY = "LAST_1_DAY";
+    String RANGE_LAST_1_WEEK = "LAST_1_WEEK";
+    String RANGE_LAST_1_MONTH = "LAST_1_MONTH";
+    String RANGE_LAST_1_QUARTER = "LAST_1_QUARTER";
+    String RANGE_HALF_1_YEAR = "HALF_1_YEAR";
+    String RANGE_LAST_1_YEAR = "LAST_1_YEAR";
     String RANG_BEGIN_END_TIME = "BEGIN_END_TIME";
 
     String INTERVAL_ONE_MINUTE = "ONE_MINUTE";
@@ -30,33 +30,51 @@ public interface DataStoreStatisticHistoryService {
 
     /**
      * 获取VMFS历史性能数据
-     *
-     * @param params
+     * @param params key required: obj_ids, indicator_ids, range
      * @return
      */
-    Map<String, Object> queryVmfsStatistic(Map<String, Object> params);
+    Map<String, Object> queryVmfsStatistic(Map<String, Object> params) throws Exception;
 
     /**
      * 获取VMFS当前性能数据值
-     *
-     * @param params
+     * @param params key required: obj_ids
      * @return
      */
-    Map<String, Object> queryVmfsStatisticCurrent(Map<String, Object> params);
+    Map<String, Object> queryVmfsStatisticCurrent(Map<String, Object> params) throws Exception;
 
     /**
      * 获取NFS历史性能数据值
-     *
-     * @param params
+     * @param params key required: obj_ids, indicator_ids, range
      * @return
      */
-    Map<String, Object> queryNfsStatistic(Map<String, Object> params);
+    Map<String, Object> queryNfsStatistic(Map<String, Object> params) throws Exception;
 
     /**
-     * 获取NFS当前性能数据值
-     *
-     * @param params
+     * 获取NFS当前性能数据
+     * @param params key required: obj_ids
      * @return
      */
-    Map<String, Object> queryNfsStatisticCurrent(Map<String, Object> params);
+    Map<String, Object> queryNfsStatisticCurrent(Map<String, Object> params) throws Exception;
+
+    /**
+     * 查询磁盘(卷)历史性能数据
+     * @param params key required: obj_ids, indicator_ids, range
+     * @return
+     */
+    Map<String, Object> queryVolumeStatistic(Map<String, Object> params) throws Exception;
+
+    /**
+     * 查询fileSystem历史性能数据(Nfsdatasotrage)
+     * @param params key required: obj_ids, indicator_ids, range
+     * @return
+     */
+    Map<String, Object> queryFsStatistic(Map<String, Object> params) throws Exception;
+
+    /**
+     * 查询服务等级历史性能数据
+     * @param params key required: obj_ids, indicator_ids, range
+     * @return
+     * @throws Exception
+     */
+    Map<String, Object> queryServiceLevelStatistic(Map<String, Object> params) throws Exception;
 }
--- dmestore-service/src/main/java/com/dmeplugin/dmestore/services/DataStoreStatisticHistoryServiceImpl.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/services/DataStoreStatisticHistoryServiceImpl.java
@@ -21,7 +21,7 @@ import java.util.*;
 public class DataStoreStatisticHistoryServiceImpl implements DataStoreStatisticHistoryService {
     private static final Logger log = LoggerFactory.getLogger(DataStoreStatisticHistoryService.class);
 
-    private Gson gson=new Gson();
+    private Gson gson = new Gson();
     @Autowired
     private DmeAccessService dmeAccessService;
 
@@ -76,83 +76,133 @@ public class DataStoreStatisticHistoryServiceImpl implements DataStoreStatisticH
     }
 
     @Override
-    public Map<String, Object> queryVmfsStatisticCurrent(Map<String, Object> params) {
+    public Map<String, Object> queryVmfsStatisticCurrent(Map<String, Object> params) throws Exception {
         Map<String, Object> remap = new HashMap<>();
-
-        //以下为模拟响应报文的处理
         Object obj_ids = params.get("obj_ids");
-        if (null != obj_ids) {
+        Object indicatorIds = params.get("indicator_ids");
+        //以下为模拟响应报文的处理
+        /*if (null != obj_ids) {
             List<String> volumeIds = (List<String>) obj_ids;
             JsonObject dataJson = vmfsStatisticCurrentMimic(volumeIds);
             remap.put("code", 200);
             remap.put("message", "queryStatistic success!");
             remap.put("data", dataJson);
-        }
+        }*/
 
         //以下为实际消息的处理
-       /*
-        params.put("obj_type_id", "1125921381679104");//SYS_LUN
-        params.put("range", " LAST_5_MINUTE");
-        Map<String, Object> currenResMap = queryVmfsStatistic(params);
-        String code = currenResMap.get("code").toString();
-        if("200".equals(code)){
-            Object object = currenResMap.get("data");
-            JsonObject volumeRes = new JsonObject();
-            JsonObject dataJson =  new JsonParser().parse(object.toString()).getAsJsonObject();
-            Set<Map.Entry<String, JsonElement>> volumeSet = dataJson.getAsJsonObject().entrySet();
-            for(Map.Entry<String, JsonElement> volume : volumeSet){
-                JsonObject countRes = new JsonObject();
-                String volume_id = volume.getKey();
-                JsonObject counterObj= volume.getValue().getAsJsonObject();
-                Set<Map.Entry<String, JsonElement>> counterSet = counterObj.getAsJsonObject().entrySet();
-                for(Map.Entry<String, JsonElement> countere : counterSet){
-                    String counter_id = countere.getKey();
-                    JsonObject counterjson = countere.getValue().getAsJsonObject();
-                    JsonArray series = counterjson.getAsJsonArray("series");
-                    for(JsonElement elment : series){
-                        JsonObject serieJson = elment.getAsJsonObject();
-                        Set<Map.Entry<String, JsonElement>> serieJsonSet = serieJson.getAsJsonObject().entrySet();
-                        for(Map.Entry<String, JsonElement> serie : serieJsonSet){
-                            String value = serie.getValue().getAsString();
-                            countRes.addProperty(counter_id,value);
-                            break;
-                        }
-                        break;
-                    }
-                }
-                volumeRes.add(volume_id,countRes);
-            }
-            remap.put("data",volumeRes);
+        if (null == indicatorIds) {
+            indicatorIds = initVolumeIndicator();
+            params.put("indicator_ids", indicatorIds);
         }
-        //else 不做处理,原始返回*/
-
+        params.put("range", RANGE_LAST_5_MINUTE);
+        params.put("interval", INTERVAL_ONE_MINUTE);
+        Map<String, Object> resp = queryStatisticByObjType("VMFS volume", params);
+        String code = resp.get("code").toString();
+        if ("200".equals(code)) {
+            Object data = remap.get("data");
+            data = getCurrentStatistic(data);
+            remap.put("data", data);
+        }
         return remap;
     }
 
     @Override
-    public Map<String, Object> queryNfsStatistic(Map<String, Object> params) {
-        return queryVmfsStatistic(params);
+    public Map<String, Object> queryNfsStatistic(Map<String, Object> params) throws Exception {
+        return queryFsStatistic(params);
     }
 
     @Override
-    public Map<String, Object> queryNfsStatisticCurrent(Map<String, Object> params) {
+    public Map<String, Object> queryNfsStatisticCurrent(Map<String, Object> params) throws Exception {
         Map<String, Object> remap = new HashMap<>();
 
-        //以下为模拟响应报文的处理
         Object obj_ids = params.get("obj_ids");
-        if (null != obj_ids) {
+        Object indicatorIds = params.get("indicator_ids");
+        //以下为模拟响应报文的处理
+       /* if (null != obj_ids) {
             List<String> volumeIds = (List<String>) obj_ids;
             JsonObject dataJson = vmfsStatisticCurrentMimic(volumeIds);
             remap.put("code", 200);
             remap.put("message", "queryStatistic fs success!");
             remap.put("data", dataJson);
-        }
+        }*/
 
         //以下为实际响应报文处理
-        //params.put("obj_type_id", "XXXXXX");//fs的obj_type_id
-
-
+        if (null == indicatorIds) {
+            indicatorIds = initFsIndicator();
+            params.put("indicator_ids", indicatorIds);
+        }
+        params.put("range", RANGE_LAST_5_MINUTE);
+        params.put("interval", INTERVAL_ONE_MINUTE);
+        Map<String, Object> resp = queryStatisticByObjType("NFS", params);
+        String code = resp.get("code").toString();
+        if ("200".equals(code)) {
+            Object data = remap.get("data");
+            data = getCurrentStatistic(data);
+            remap.put("data", data);
+        }
         return remap;
+    }
+
+    //查询卷的性能
+    @Override
+    public Map<String, Object> queryVolumeStatistic(Map<String, Object> params) throws Exception {
+        String obj_type_id = "1125921381679104";//SYS_Lun
+        params.put("obj_type_id", obj_type_id);
+        return queryStatisticByObjType("volume", params);
+    }
+
+    //查询FS的性能(NFS)
+    @Override
+    public Map<String, Object> queryFsStatistic(Map<String, Object> params) throws Exception {
+        Object indicatorIds = params.get("indicator_ids");
+        String obj_type_id = "1125904201809920";//SYS_???  使用存储设备? SYS_StorDevice 1125904201809920
+        params.put("obj_type_id", obj_type_id);
+        if (null == indicatorIds) {
+            indicatorIds = initFsIndicator();
+            params.put("indicator_ids", indicatorIds);
+        }
+        return queryStatisticByObjType("fs", params);
+    }
+
+    @Override
+    public Map<String, Object> queryServiceLevelStatistic(Map<String, Object> params) throws Exception {
+        Object indicatorIds = params.get("indicator_ids");
+        String obj_type_id = "1126174784749568";//SYS_DjTier
+        params.put("obj_type_id", obj_type_id);
+        if (null == indicatorIds) {
+            indicatorIds = initServiceLevelIndicator();
+            params.put("indicator_ids", indicatorIds);
+        }
+        return queryStatisticByObjType("serviceLevel", params);
+    }
+
+    //query statistic by objType
+    private Map<String, Object> queryStatisticByObjType(String objectType, Map<String, Object> params) throws Exception {
+        Map<String, Object> resmap = new HashMap<>();
+        resmap.put("code", 200);
+        resmap.put("message", "query" + objectType + "Statistic success!");
+        resmap.put("data", params);
+
+        ResponseEntity responseEntity;
+        Object statisticObj;
+        try {
+            responseEntity = queryStatistic(params);
+            if (null != responseEntity && 200 == responseEntity.getStatusCodeValue()) {
+                Object body = responseEntity.getBody();
+                JsonObject bodyJson = new JsonParser().parse(body.toString()).getAsJsonObject();
+                statisticObj = bodyJson.get("data").getAsJsonObject();
+                resmap.put("data", statisticObj);
+            } else {
+                resmap.put("code", 503);
+                resmap.put("message", "query" + objectType + "Statistic error!");
+                log.error("query" + objectType + "Statistic error,the params is:{}", gson.toJson(params));
+            }
+        } catch (Exception e) {
+            resmap.put("code", 503);
+            resmap.put("message", "query" + objectType + "Statistic exception!");
+            log.error("query" + objectType + "Statistic exception.", e);
+        }
+        return resmap;
     }
 
     //query statistic
@@ -161,12 +211,14 @@ public class DataStoreStatisticHistoryServiceImpl implements DataStoreStatisticH
         String objTypeId = params.get("obj_type_id").toString();
         Object indicatorIds = params.get("indicator_ids");
         Object objIds = params.get("obj_ids");
+        params = initParams(params);
+
         String interval = params.get("interval").toString();
         String range = params.get("range").toString();
         String beginTime = params.get("begin_time").toString();
         String endTime = params.get("end_time").toString();
 
-        //参数预处理 效验赋值 处理资源对象类型指标 指标id name转换等
+        //参数预处理 效验赋值 处理资源对象类型指标 指标id name转换等 目前要求按API格式传参,暂不效验
         //parseParams(); ......
 
         Map<String, Object> requestbody = new HashMap<>();
@@ -175,10 +227,8 @@ public class DataStoreStatisticHistoryServiceImpl implements DataStoreStatisticH
         requestbody.put("obj_ids", objIds);
         requestbody.put("interval", interval);
         requestbody.put("range", range);
-        if (!StringUtils.isEmpty(range) && "BEGIN_END_TIME".equals(range)) {
-            requestbody.put("begin_time", beginTime);
-            requestbody.put("end_time", endTime);
-        }
+        requestbody.put("begin_time", beginTime);
+        requestbody.put("end_time", endTime);
         responseEntity = dmeAccessService.access(STATISTIC_QUERY, HttpMethod.POST, requestbody.toString());
 
         return responseEntity;
@@ -264,20 +314,140 @@ public class DataStoreStatisticHistoryServiceImpl implements DataStoreStatisticH
     }
 
     //性能查询条件
-    private Map<String, Object> queryParamsDefaulf(Map<String, Object> params){
+    private Map<String, Object> initParams(Map<String, Object> params) {
         String rang = ToolUtils.getStr(params.get("range"));
         String interval = ToolUtils.getStr(params.get("interval"));
-        String range = params.get("range").toString();
         long beginTime = ToolUtils.getLong(params.get("begin_time"));
         long endTime = ToolUtils.getLong(params.get("end_time"));
 
-
         //设置默认值
-        
+        if (StringUtils.isEmpty(rang)) {
+            rang = RANGE_LAST_1_DAY;
+            params.put("range", rang);
+        }
+        if (0 == endTime) {
+            endTime = System.currentTimeMillis();
+            params.put("end_time", endTime);
+        }
+        if (0 == beginTime) {
+            switch (rang) {
+                case RANGE_LAST_5_MINUTE:
+                    beginTime = endTime - 5 * 60 * 1000;
+                    break;
+                case RANGE_LAST_1_HOUR:
+                    beginTime = endTime - 60 * 60 * 1000;
+                    break;
+                case RANGE_LAST_1_DAY:
+                    beginTime = endTime - 24 * 60 * 60 * 1000;
+                    break;
+                case RANGE_LAST_1_WEEK:
+                    beginTime = endTime - 7 * 24 * 60 * 60 * 1000;
+                    break;
+                case RANGE_LAST_1_MONTH:
+                    beginTime = endTime - 30 * 24 * 60 * 60 * 1000;
+                    break;
+                case RANGE_LAST_1_QUARTER:
+                    beginTime = endTime - 3 * 30 * 24 * 60 * 60 * 1000;
+                    break;
+                case RANGE_HALF_1_YEAR:
+                    beginTime = endTime - 6 * 30 * 24 * 60 * 60 * 1000;
+                    break;
+                case RANGE_LAST_1_YEAR:
+                    beginTime = endTime - 365 * 24 * 60 * 60 * 1000;
+                    break;
+            }
+            params.put("begin_time", beginTime);
+        }
+        if (StringUtils.isEmpty(interval)) {
+            switch (rang) {
+                case RANGE_LAST_5_MINUTE:
+                    interval = INTERVAL_ONE_MINUTE;
+                    break;
+                case RANGE_LAST_1_HOUR:
+                    interval = INTERVAL_MINUTE;
+                    break;
+                case RANGE_LAST_1_DAY:
+                    interval = INTERVAL_MINUTE;
+                    break;
+                case RANGE_LAST_1_WEEK:
+                    interval = INTERVAL_HALF_HOUR;
+                    break;
+                case RANGE_LAST_1_MONTH:
+                    interval = INTERVAL_HOUR;
+                    break;
+                case RANGE_LAST_1_QUARTER:
+                    interval = INTERVAL_DAY;
+                    break;
+                case RANGE_HALF_1_YEAR:
+                    interval = INTERVAL_DAY;
+                    break;
+                case RANGE_LAST_1_YEAR:
+                    interval = INTERVAL_DAY;
+                    break;
+            }
+            params.put("interval", interval);
+        }
         return params;
     }
 
+    //nfs的默认指标集合 目前取的DME存储设备的指标
+    private List<String> initFsIndicator() {
+        List<String> indicators = new ArrayList<>();
+        indicators.add("1125904201875461");//readThroughput
+        indicators.add("1125904201875462");//writeThroughput
+        indicators.add("1125904201875459");//readBandwidth
+        indicators.add("1125904201875460");//writeBandwidth
+        indicators.add("1125904201875464");//responseTime 平均IO响应时间
+        return indicators;
+    }
 
+    //volume默认指标集合
+    private List<String> initVolumeIndicator() {
+        List<String> indicators = new ArrayList<>();
+        indicators.add("1125921381744648");//readThroughput 读IOPS
+        indicators.add("1125921381744649");//writeThroughput
+        indicators.add("1125921381744646");//readBandwidth
+        indicators.add("1125921381744647");//writeBandwidth
+        indicators.add("1125921381744656");//readResponseTime
+        indicators.add("1125921381744656");//writeResponseTime
+        return indicators;
+    }
 
+    //serviceLevel默认指标集合
+    private List<String> initServiceLevelIndicator() {
+        List<String> indicators = new ArrayList<>();
+        indicators.add("1126174784815111");//maxResponseTime
+        indicators.add("1126174784815118");//bandwidthTiB
+        return indicators;
+    }
 
+    //消息转换  提取实时性能数据
+    private JsonObject getCurrentStatistic(Object object) {
+        JsonObject data = new JsonObject();
+        JsonObject dataJson = new JsonParser().parse(object.toString()).getAsJsonObject();
+        Set<Map.Entry<String, JsonElement>> volumeSet = dataJson.getAsJsonObject().entrySet();
+        for (Map.Entry<String, JsonElement> volume : volumeSet) {
+            JsonObject countRes = new JsonObject();
+            String volume_id = volume.getKey();
+            JsonObject counterObj = volume.getValue().getAsJsonObject();
+            Set<Map.Entry<String, JsonElement>> counterSet = counterObj.getAsJsonObject().entrySet();
+            for (Map.Entry<String, JsonElement> countere : counterSet) {
+                String counter_id = countere.getKey();
+                JsonObject counterjson = countere.getValue().getAsJsonObject();
+                JsonArray series = counterjson.getAsJsonArray("series");
+                for (JsonElement elment : series) {
+                    JsonObject serieJson = elment.getAsJsonObject();
+                    Set<Map.Entry<String, JsonElement>> serieJsonSet = serieJson.getAsJsonObject().entrySet();
+                    for (Map.Entry<String, JsonElement> serie : serieJsonSet) {
+                        String value = serie.getValue().getAsString();
+                        countRes.addProperty(counter_id, value);
+                        break;
+                    }
+                    break;
+                }
+            }
+            data.add(volume_id, countRes);
+        }
+        return data;
+    }
 }
