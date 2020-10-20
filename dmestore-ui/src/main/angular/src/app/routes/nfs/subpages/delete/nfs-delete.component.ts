fbe6b83cc91bd84d86b7c8ac836dc5945048e70b
yuanwei <wei.yuan@gh-ca.com>
2020-10-20 20:45:45 +0800

    nfs 部分页面路由调整

--- dmestore-ui/src/main/angular/src/app/routes/nfs/subpages/delete/nfs-delete.component.ts
+++ dmestore-ui/src/main/angular/src/app/routes/nfs/subpages/delete/nfs-delete.component.ts
@@ -10,12 +10,47 @@ import {NfsDeleteService} from "./nfs-delete.service";
   providers: [GlobalsService,NfsDeleteService]
 })
 export class NfsDeleteComponent implements OnInit{
-
+  viewPage: string;
+  pluginFlag: string;//来至插件的标记
+  dataStoreObjectId:string;
+  errorMsg: string;
   constructor(private deleteService: NfsDeleteService, private cdr: ChangeDetectorRef,
               private gs: GlobalsService,
               private activatedRoute: ActivatedRoute,private router:Router){
   }
   ngOnInit(): void {
+    const ctx = this.gs.getClientSdk().app.getContextObjects();
+    //入口是DataSource
+    this.viewPage='add_plugin'
+    this.activatedRoute.queryParams.subscribe(queryParam => {
+      this.dataStoreObjectId = queryParam.objectid;
+      this.pluginFlag =queryParam.flag;
+    });
+    if(this.pluginFlag==null){
+      //入口来至Vcenter
+      //this.dsObjectId=ctx[0].id;
+      this.viewPage='delete_vcenter'
+    }
+  }
+  delNfs(){
+    this.deleteService.delNfs(this.dataStoreObjectId).subscribe((result: any) => {
+      if (result.code === '200'){
+        if(this.pluginFlag=='plugin'){
+          this.backToNfsList();
+        }else{
+          this.closeModel();
+        }
+      }else{
+        this.errorMsg = '编辑失败！'+result.description;
+      }
+    });
   }
 
+  backToNfsList(){
+    this.router.navigate(['nfs']);
+  }
+
+  closeModel(){
+    this.gs.getClientSdk().modal.close();
+  }
 }
--- dmestore-ui/src/main/angular/src/app/routes/nfs/subpages/expand/nfs-expand.service.ts
+++ dmestore-ui/src/main/angular/src/app/routes/nfs/subpages/expand/nfs-expand.service.ts
@@ -4,5 +4,8 @@ import {HttpClient} from "@angular/common/http";
 @Injectable()
 export class NfsExpandService{
   constructor(private http: HttpClient) {}
+  changeCapacity(params= {}){
+    return this.http.post('operatenfs/changenfsdatastore', params);
+  }
 }
 
