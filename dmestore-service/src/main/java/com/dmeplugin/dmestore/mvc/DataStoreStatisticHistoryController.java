8c2642969873ab7e0ed8f6aa1dca9b65ccf0fcee
liuxh <xiaohuan.liu@gh-ca.com>
2020-10-22 18:43:58 +0800

    新增控制器 存储端口存储硬盘的性能接口

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/DataStoreStatisticHistoryController.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/mvc/DataStoreStatisticHistoryController.java
@@ -35,7 +35,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/vmfs", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getVmfsVolumeStatistic(@RequestBody Map<String, Object> params)   {
+    public ResponseBodyBean getVmfsVolumeStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/vmfs params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryVmfsStatistic(params));
@@ -54,7 +54,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/volume", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getVolumeStatistic(@RequestBody Map<String, Object> params)   {
+    public ResponseBodyBean getVolumeStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/volume params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryVolumeStatistic(params));
@@ -74,7 +74,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/nfs", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getNfsVolumeStatistic(@RequestBody Map<String, Object> params)   {
+    public ResponseBodyBean getNfsVolumeStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/nfs params==" + gson.toJson(params));
 
         try {
@@ -96,7 +96,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/fs", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getFsStatistic(@RequestBody Map<String, Object> params)   {
+    public ResponseBodyBean getFsStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/fs params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryFsStatistic(params));
@@ -115,7 +115,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/servicelevel", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getServiceLevelStatistic(@RequestBody Map<String, Object> params)  {
+    public ResponseBodyBean getServiceLevelStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/servicelevel params==" + gson.toJson(params));
 
         try {
@@ -136,7 +136,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/servicelevelLun", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getServiceLevelLunStatistic(@RequestBody Map<String, Object> params)  {
+    public ResponseBodyBean getServiceLevelLunStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/servicelevellun params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryServiceLevelLunStatistic(params));
@@ -156,7 +156,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/servicelevelStoragePool", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getServiceLevelStoragePoolStatistic(@RequestBody Map<String, Object> params)   {
+    public ResponseBodyBean getServiceLevelStoragePoolStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/servicelevelstoragepool params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryServiceLevelStoragePoolStatistic(params));
@@ -195,7 +195,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/storagePoolCurrent", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getStoragePoolCurrentStatistic(@RequestBody Map<String, Object> params)   {
+    public ResponseBodyBean getStoragePoolCurrentStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/storagepoolcurrent params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryStoragePoolCurrentStatistic(params));
@@ -215,7 +215,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/storageDevice", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getStoragDeviceStatistic(@RequestBody Map<String, Object> params)  {
+    public ResponseBodyBean getStoragDeviceStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/storagedevice params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryStorageDevcieStatistic(params));
@@ -223,7 +223,6 @@ public class DataStoreStatisticHistoryController extends BaseController {
             e.printStackTrace();
             return failure(e.getMessage());
         }
-
     }
 
     /**
@@ -235,7 +234,7 @@ public class DataStoreStatisticHistoryController extends BaseController {
      */
     @RequestMapping(value = "/storageDeviceCurrent", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseBodyBean getStoragDeviceCurrenStatistic(@RequestBody Map<String, Object> params)  {
+    public ResponseBodyBean getStoragDeviceCurrentStatistic(@RequestBody Map<String, Object> params) {
         LOG.info("datastorestatistichistrory/storagedevicecurrent params==" + gson.toJson(params));
         try {
             return success(dataSotreStatisticHistroyService.queryStorageDevcieCurrentStatistic(params));
@@ -243,9 +242,122 @@ public class DataStoreStatisticHistoryController extends BaseController {
             e.printStackTrace();
             return failure(e.getMessage());
         }
+    }
+
+    /**
+     * 查询控制器历史性能,对象Id为控制器设备Id  eg:{"obj_ids" : ["cd17eca4-3056-4ca0-9a4a-2338174c33d8"]}
+     *
+     * @param params key required: obj_ids, indicator_ids, range (obj_ids, contorllerIds)
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/controller", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean getControllerStatistic(@RequestBody Map<String, Object> params) {
+        LOG.info("datastorestatistichistrory/controllercurrent params==" + gson.toJson(params));
+        try {
+            return success(dataSotreStatisticHistroyService.queryControllerStatistic(params));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return failure(e.getMessage());
+        }
+    }
+
+    /**
+     * 查询控制器当前性能,对象Id为控制器设备Id  eg:{"obj_ids" : ["cd17eca4-3056-4ca0-9a4a-2338174c33d8"]}
+     *
+     * @param params key required: obj_ids, indicator_ids, range (obj_ids, contorllerIds)
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/controllerCurrent", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean getControllerCurrenStatistic(@RequestBody Map<String, Object> params) {
+        LOG.info("datastorestatistichistrory/controllercurrent params==" + gson.toJson(params));
+        try {
+            return success(dataSotreStatisticHistroyService.queryControllerCurrentStatistic(params));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return failure(e.getMessage());
+        }
 
     }
 
+    /**
+     * 查询存储端口历史性能,对象Id为存储端口Id  eg:{"obj_ids" : ["cd17eca4-3056-4ca0-9a4a-2338174c33d8"]}
+     *
+     * @param params key required: obj_ids, indicator_ids, range (obj_ids, storagedeviceIds)
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/storagePort", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean getStoragPortStatistic(@RequestBody Map<String, Object> params) {
+        LOG.info("datastorestatistichistrory/storageport params==" + gson.toJson(params));
+        try {
+            return success(dataSotreStatisticHistroyService.queryStoragePortStatistic(params));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return failure(e.getMessage());
+        }
+    }
 
+    /**
+     * 查询存储端口当前性能,对象Id为存储端口Id  eg:{"obj_ids" : ["cd17eca4-3056-4ca0-9a4a-2338174c33d8"]}
+     *
+     * @param params key required: obj_ids, indicator_ids, range (obj_ids, storagedeviceIds)
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/storagePortCurrent", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean getStoragPortCurrentStatistic(@RequestBody Map<String, Object> params) {
+        LOG.info("datastorestatistichistrory/storageportcurrent params==" + gson.toJson(params));
+        try {
+            return success(dataSotreStatisticHistroyService.queryStoragePortCurrentStatistic(params));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return failure(e.getMessage());
+        }
+    }
+
+
+    /**
+     * 查询存储磁盘历史性能,对象Id为存储磁盘Id  eg:{"obj_ids" : ["cd17eca4-3056-4ca0-9a4a-2338174c33d8"]}
+     *
+     * @param params key required: obj_ids, indicator_ids, range (obj_ids, storagedeviceIds)
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/storageDisk", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean getStoragDiskStatistic(@RequestBody Map<String, Object> params) {
+        LOG.info("datastorestatistichistrory/storagedisk params==" + gson.toJson(params));
+        try {
+            return success(dataSotreStatisticHistroyService.queryStorageDiskStatistic(params));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return failure(e.getMessage());
+        }
+    }
+
+    /**
+     * 查询存储磁盘当前性能,对象Id为存储磁盘Id  eg:{"obj_ids" : ["cd17eca4-3056-4ca0-9a4a-2338174c33d8"]}
+     *
+     * @param params key required: obj_ids, indicator_ids, range (obj_ids, storagedeviceIds)
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/storageDiskCurrent", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseBodyBean getStoragDiskCurrentStatistic(@RequestBody Map<String, Object> params) {
+        LOG.info("datastorestatistichistrory/storagediskcurrent params==" + gson.toJson(params));
+        try {
+            return success(dataSotreStatisticHistroyService.queryStorageDiskCurrentStatistic(params));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return failure(e.getMessage());
+        }
+    }
 
 }
