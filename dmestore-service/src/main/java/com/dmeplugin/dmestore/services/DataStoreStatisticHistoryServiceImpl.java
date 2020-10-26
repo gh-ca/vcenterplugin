f9c0600a23d58d8b52e9dbf8f6478035cf5d1489
liuxh <xiaohuan.liu@gh-ca.com>
2020-10-26 10:08:33 +0800

    修复端口状态扫描慢的问题

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/services/DataStoreStatisticHistoryServiceImpl.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/services/DataStoreStatisticHistoryServiceImpl.java
@@ -917,7 +917,6 @@ public class DataStoreStatisticHistoryServiceImpl implements DataStoreStatisticH
             indicators.add(DmeIndicatorConstants.COUNTER_ID_STORAGEPORT_THROUGHPUT);//throughput
             indicators.add(DmeIndicatorConstants.COUNTER_ID_STORAGEPORT_BANDWIDTH);//bandwidth
             indicators.add(DmeIndicatorConstants.COUNTER_ID_STORAGEPORT_UTILITY);//利用率
-            indicators.add(DmeIndicatorConstants.COUNTER_ID_STORAGEPORT_THROUGHPUT);//IOPS
         } else {
             indicators.add(DmeIndicatorConstants.COUNTER_ID_STORAGEPORT_READTHROUGHPUT);//readThroughput 读IOPS
             indicators.add(DmeIndicatorConstants.COUNTER_ID_STORAGEPORT_WRITETHROUGHPUT);//writeThroughput
