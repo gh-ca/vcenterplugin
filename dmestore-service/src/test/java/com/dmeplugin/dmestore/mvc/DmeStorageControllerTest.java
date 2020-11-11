14cc6c2f0afa83acef00ff7eea1a80df55fe8a78
lianq <qiang.lian@gh-ca.com>
2020-11-11 16:41:41 +0800

    dmestorage Controller单元测试

---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/mvc/DmeStorageControllerTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className DmeStorageControllerTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/11 14:08
+ */public class DmeStorageControllerTest {
+  
+}
---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/mvc/NfsOperationControllerTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className NfsOperationControllerTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/11 10:38
+ */public class NfsOperationControllerTest {
+  
+}
