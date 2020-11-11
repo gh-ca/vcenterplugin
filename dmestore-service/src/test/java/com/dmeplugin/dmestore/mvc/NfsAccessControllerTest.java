3a052fee693bfcc362e669b5aeb85f9d31c20351
lianq <qiang.lian@gh-ca.com>
2020-11-11 18:03:08 +0800

    单元测试

---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/mvc/NfsAccessControllerTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className NfsAccessControllerTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/11 17:49
+ */public class NfsAccessControllerTest {
+  
+}
---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/mvc/NfsDataStoreControllerTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className NfsDataStoreControllerTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/11 17:51
+ */public class NfsDataStoreControllerTest {
+  
+}
