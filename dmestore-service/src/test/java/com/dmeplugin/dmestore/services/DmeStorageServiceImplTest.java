3522809bbea7d694dd73eea1b1363cef280e9c2b
lianq <qiang.lian@gh-ca.com>
2020-11-12 18:14:25 +0800

    单元测试

---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/services/DmeStorageServiceImplTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className DmeStorageServiceImplTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/12 10:09
+ */public class DmeStorageServiceImplTest {
+  
+}
