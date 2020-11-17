9fa4996ecf64239d8e1be9c39d717d9709dc60ec
lianq <qiang.lian@gh-ca.com>
2020-11-17 17:59:57 +0800

    单元测试

---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/mvc/HostAccessControllerTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className HostAccessControllerTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/17 17:55
+ */public class HostAccessControllerTest {
+  
+}
