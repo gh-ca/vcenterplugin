47e195b67494faf7a41bfc78d2167abdf1a66761
lianq <qiang.lian@gh-ca.com>
2020-11-24 14:36:30 +0800

    单元测试

---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/model/LogicPortsTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className LogicPortsTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/24 10:15
+ */public class LogicPortsTest {
+  
+}
--- dmestore-service/src/test/java/com/dmeplugin/dmestore/mvc/DmeAccessControllerTest.java
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/mvc/DmeAccessControllerTest.java
@@ -1,62 +1,9 @@
-package com.dmeplugin.dmestore.mvc;
-
-import com.dmeplugin.dmestore.dao.DmeInfoDao;
-import com.dmeplugin.dmestore.services.DmeAccessServiceImpl;
-import com.google.gson.Gson;
-import org.junit.Test;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class DmeAccessControllerTest {
-    private Gson gson = new Gson();
-
-    @Test
-    public void controller_accessTest() throws Exception {
-        DmeAccessController controller = new DmeAccessController();
-        Map<String, Object> params = new HashMap<>();
-        params.put("hostIp", "10.143.133.200");
-        params.put("hostPort", 26335);
-        params.put("userName", "evuser");
-        params.put("password", "Pbu421234");
-        System.out.println(gson.toJson(params));
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        mockMvc.perform(MockMvcRequestBuilders.post("/accessdme/access").contentType(MediaType.APPLICATION_JSON).content(gson.toJson(params)))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
-
-    @Test
-    public void controller_refreshaccessTest() throws Exception {
-        DmeAccessController controller = new DmeAccessController();
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/accessdme/refreshaccess"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
-
-    @Test
-    public void service_accessDme_Test() throws Exception {
-        DmeAccessServiceImpl dmeAccessService = new DmeAccessServiceImpl();
-        DmeInfoDao dmeInfoDao = new DmeInfoDao();
-        dmeAccessService.setDmeInfoDao(dmeInfoDao);
-        Map<String, Object> params = new HashMap<>();
-        params.put("hostIp", "10.143.133.200");
-        params.put("hostPort", 26335);
-        params.put("userName", "evuser");
-        params.put("password", "Pbu421234");
-        dmeAccessService.accessDme(params);
-    }
-
-    @Test
-    public void service_refreshDme_Test() throws Exception {
-        DmeAccessServiceImpl dmeAccessService = new DmeAccessServiceImpl();
-        DmeInfoDao dmeInfoDao = new DmeInfoDao();
-        dmeAccessService.setDmeInfoDao(dmeInfoDao);
-        Map<String, Object> remap = dmeAccessService.refreshDme();
-        System.out.println("remap==" + remap);
-    }
+import static org.junit.Assert.*;
+/**
+ *@className DmeAccessControllerTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/24 11:45
+ */public class DmeAccessControllerTest {
+  
 }
