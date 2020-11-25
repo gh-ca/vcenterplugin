059d3fa5fdae85b949a4c935f51059e608f9252d
lianq <qiang.lian@gh-ca.com>
2020-11-25 16:45:28 +0800

    单元测试

---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/utils/VCSDKUtilsTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className VCSDKUtilsTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/25 15:51
+ */public class VCSDKUtilsTest {
+  
+}
---
+++ dmestore-service/src/test/java/com/dmeplugin/vmware/mo/ClusterMOTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className ClusterMOTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/11/25 15:28
+ */public class ClusterMOTest {
+  
+}
