5043e9703d9b7d19137e0cd0524d6e44c5a0b2a3
lianq <qiang.lian@gh-ca.com>
2020-12-02 21:04:35 +0800

    dao层单元测试

--- dmestore-service/src/test/java/com/dmeplugin/dmestore/dao/BestPracticeCheckDaoTest.java
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/dao/BestPracticeCheckDaoTest.java
@@ -1,71 +1,9 @@
-package com.dmeplugin.dmestore.dao;
-
-import static org.mockito.Mockito.mock;
-
-import com.dmeplugin.dmestore.model.BestPracticeBean;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.junit.Assert.*;
 /**
- * @author admin
- * @version 1.0.0
- * @ClassName BestPracticeCheckDaoTest.java
- * @Description TODO
- * @createTime 2020年12月01日 14:11:00
- */
-public class BestPracticeCheckDaoTest {
-
-    @InjectMocks
-    private BestPracticeCheckDao bestPracticeCheckDao;
-
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    public void save() {
-        List<BestPracticeBean> list = mock(ArrayList.class);
-        BestPracticeBean bestPracticeBean = new BestPracticeBean();
-        list.add(bestPracticeBean);
-        bestPracticeCheckDao.save(list);
-    }
-
-    @Test
-    public void getHostNameByHostsetting() {
-    }
-
-    @Test
-    public void getAllHostIds() {
-    }
-
-    @Test
-    public void getByHostIds() {
-    }
-
-    @Test
-    public void getRecordByPage() {
-    }
-
-    @Test
-    public void getRecordBeanByHostsetting() {
-    }
-
-    @Test
-    public void deleteBy() {
-    }
-
-    @Test
-    public void deleteByHostNameAndHostsetting() {
-    }
-
-    @Test
-    public void update() {
-    }
+ *@className BestPracticeCheckDaoTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/12/2 20:09
+ */public class BestPracticeCheckDaoTest {
+  
 }
---
+++ dmestore-service/src/test/java/com/dmeplugin/dmestore/dao/VCenterInfoDaoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+/**
+ *@className VCenterInfoDaoTest
+ *@description  TODO
+ *@author lianq
+ *@date 2020/12/2 20:55
+ */public class VCenterInfoDaoTest {
+  
+}
