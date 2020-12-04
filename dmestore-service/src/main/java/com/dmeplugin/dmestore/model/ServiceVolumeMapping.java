9cc6a32ecb957da498e802ba31147fb095fe90da
lianq <qiang.lian@gh-ca.com>
2020-12-04 15:43:00 +0800

    checkstyle

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/model/ServiceVolumeMapping.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/model/ServiceVolumeMapping.java
@@ -4,22 +4,43 @@ package com.dmeplugin.dmestore.model;
  * @author wangxiangyong
  **/
 public class ServiceVolumeMapping {
+    /**
+     * hostId .
+     */
     private String hostId;
+    /**
+     * hostgroupId .
+     */
     private String hostgroupId;
 
+    /**
+     * getHostId .
+     * @return String .
+     */
     public String getHostId() {
         return hostId;
     }
 
-    public void setHostId(String hostId) {
-        this.hostId = hostId;
+    /**
+     * setHostId .
+     * @param param .
+     */
+    public void setHostId(final String param) {
+        this.hostId = param;
     }
-
+    /**
+     * getHostgroupId .
+     * @return String .
+     */
     public String getHostgroupId() {
         return hostgroupId;
     }
 
-    public void setHostgroupId(String hostgroupId) {
-        this.hostgroupId = hostgroupId;
+    /**
+     * setHostgroupId .
+     * @param param .
+     */
+    public void setHostgroupId(final String param) {
+        this.hostgroupId = param;
     }
 }
