0ff0d9c8f28e49539557c64ee2ccd4ad764399b5
lianq <qiang.lian@gh-ca.com>
2020-12-04 14:30:34 +0800

    checkstyle

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/model/CustomizeVolumeTuning.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/model/CustomizeVolumeTuning.java
@@ -6,57 +6,94 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author lianq
  * @ClassName: CustomizeVolumeTuning
  * @Company: GH-CA
- * @create 2020-09-03
+ * @since  2020-09-03
  */
 public class CustomizeVolumeTuning {
-
+    /**
+     * smartQos .
+     */
     private SmartQos smartQos;
+    /**
+     * compressionEnabled .
+     */
     @JsonProperty(value = "compression_enabled")
     private Boolean compressionEnabled;
+    /**
+     * dedupeEnabled .
+     */
     @JsonProperty(value = "dedupe_enabled")
     private Boolean dedupeEnabled;
-    //数据迁移策略，取值范围 0：不迁移，1：自动迁移，2：向高性能层迁移，3：向低性能层迁移  默认值：不迁移
+    /**
+     * 数据迁移策略，取值范围 0：不迁移，1：自动迁移，
+     * 2：向高性能层迁移，3：向低性能层迁移  默认值：不迁移 .
+     */
     private Integer smarttier;
 
+    /**
+     * getSmartQos .
+     * @return SmartQos .
+     */
     public SmartQos getSmartQos() {
         return smartQos;
     }
 
-    public void setSmartQos(SmartQos smartQos) {
-        this.smartQos = smartQos;
+    /**
+     * setSmartQos .
+     * @param param .
+     */
+    public void setSmartQos(final SmartQos param) {
+        this.smartQos = param;
     }
-
+    /**
+     * getCompressionEnabled .
+     * @return Boolean .
+     */
     public Boolean getCompressionEnabled() {
         return compressionEnabled;
     }
-
-    public void setCompressionEnabled(Boolean compressionEnabled) {
-        this.compressionEnabled = compressionEnabled;
+    /**
+     * setSmartQos .
+     * @param param .
+     */
+    public void setCompressionEnabled(final Boolean param) {
+        this.compressionEnabled = param;
     }
-
+    /**
+     * getDedupeEnabled .
+     * @return Boolean .
+     */
     public Boolean getDedupeEnabled() {
         return dedupeEnabled;
     }
-
-    public void setDedupeEnabled(Boolean dedupeEnabled) {
-        this.dedupeEnabled = dedupeEnabled;
+    /**
+     * setSmartQos .
+     * @param param .
+     */
+    public void setDedupeEnabled(final Boolean param) {
+        this.dedupeEnabled = param;
     }
-
+    /**
+     * getSmarttier .
+     * @return Integer .
+     */
     public Integer getSmarttier() {
         return smarttier;
     }
-
-    public void setSmarttier(Integer smarttier) {
-        this.smarttier = smarttier;
+    /**
+     * setSmartQos .
+     * @param param .
+     */
+    public void setSmarttier(final Integer param) {
+        this.smarttier = param;
     }
 
     @Override
-    public String toString() {
-        return "CustomizeVolumeTuning{" +
-            "smartQos=" + smartQos +
-            ", compressionEnabled=" + compressionEnabled +
-            ", dedupeEnabled=" + dedupeEnabled +
-            ", smarttier=" + smarttier +
-            '}';
+    public final String toString() {
+        return "CustomizeVolumeTuning{"
+            + "smartQos=" + smartQos
+            + ", compressionEnabled=" + compressionEnabled
+            + ", dedupeEnabled=" + dedupeEnabled
+            + ", smarttier=" + smarttier
+            + '}';
     }
 }
