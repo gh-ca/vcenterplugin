066b402054383011f189e9da693ef2c14f8a0ce5
lianq <qiang.lian@gh-ca.com>
2020-12-04 16:22:49 +0800

    checkstyle

--- dmestore-service/src/main/java/com/dmeplugin/dmestore/model/SmartQosForRdmCreate.java
+++ dmestore-service/src/main/java/com/dmeplugin/dmestore/model/SmartQosForRdmCreate.java
@@ -5,15 +5,23 @@ package com.dmeplugin.dmestore.model;
  */
 public class SmartQosForRdmCreate extends BaseSmartQos {
     /**
-     * 控制策略， 0：保护IO下限，1：控制IO上限
+     * 控制策略， 0：保护IO下限，1：控制IO上限.
      **/
     private String controlPolicy;
 
+    /**
+     * getControlPolicy .
+     * @return String .
+     */
     public String getControlPolicy() {
         return controlPolicy;
     }
 
-    public void setControlPolicy(String controlPolicy) {
-        this.controlPolicy = controlPolicy;
+    /**
+     * setControlPolicy .
+     * @param param .
+     */
+    public void setControlPolicy(final String param) {
+        this.controlPolicy = param;
     }
 }
