cd9f9588b8e10bdc5e6ec5d6a4d0fd00194773c7
lianq <qiang.lian@gh-ca.com>
2020-12-11 16:22:56 +0800

    修改包名

---
+++ dmestore-deployer/src/main/java/com/huawei/vcenter/Constants.java
@@ -0,0 +1,10 @@
+package com.huawei.vcenter;
+
+
+/**
+ * @author andrewliu
+ */
+public class Constants {
+    public static final String UPDATE_FILE = "dme-vcenter-plugin.zip";
+    public static final String KEYSTORE_FILE = "tomcat.keystore";
+}
