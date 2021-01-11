d3bed03c8c6b152c5be75d5941e321a3e71c2099
wangxiangyong <111@163.com>
2021-01-11 17:56:50 +0800

    1、RMD创建，第二个磁盘创建失败问题修改 2、服务等级容量、性能接口新增

--- dmestore-service/src/main/java/com/huawei/dmestore/mvc/ServiceLevelController.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/mvc/ServiceLevelController.java
@@ -12,11 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
-
 
 /**
  * ServiceLevelController
@@ -99,6 +99,68 @@ public class ServiceLevelController extends BaseController {
     public ResponseBodyBean manualupdate() {
         try {
             serviceLevelService.updateVmwarePolicy();
+            return success();
+        } catch (DmeException e) {
+            return failure(e.getMessage());
+        }
+    }
+
+    /**
+     * statLun
+     *
+     * @return ResponseBodyBean
+     */
+    @RequestMapping(value = "/capacity/stat-lun", method = RequestMethod.GET)
+    public ResponseBodyBean statLun(@RequestParam String serviceLevelId, @RequestParam String interval) {
+        try {
+            serviceLevelService.statLunDatasetsQuery(serviceLevelId, interval);
+            return success();
+        } catch (DmeException e) {
+            return failure(e.getMessage());
+        }
+    }
+
+    /**
+     * stat-storage-pool
+     *
+     * @return ResponseBodyBean
+     */
+    @RequestMapping(value = "/capacity/stat-storage-pool", method = RequestMethod.GET)
+    public ResponseBodyBean statStoragePool(@RequestParam String serviceLevelId, @RequestParam String interval) {
+        try {
+            serviceLevelService.statStoragePoolDatasetsQuery(serviceLevelId, interval);
+            return success();
+        } catch (DmeException e) {
+            return failure(e.getMessage());
+        }
+    }
+
+    /**
+     * perf-stat-lun-details
+     *
+     * @return ResponseBodyBean
+     */
+    @RequestMapping(value = "/performance/perf-stat-lun-details", method = RequestMethod.GET)
+    public ResponseBodyBean lunPerformanceDatasetsQuery(@RequestParam String serviceLevelId,
+        @RequestParam String interval) {
+        try {
+            serviceLevelService.lunPerformanceDatasetsQuery(serviceLevelId, interval);
+            return success();
+        } catch (DmeException e) {
+            return failure(e.getMessage());
+        }
+    }
+
+    /**
+     * perf-stat-storage-pool-details
+     *
+     * @return ResponseBodyBean
+     */
+    @RequestMapping(value = "/performance/perf-stat-storage-pool-details", method = RequestMethod.GET)
+    public ResponseBodyBean storagePoolPerformanceDatasetsQuery(@RequestParam String serviceLevelId,
+        @RequestParam String interval) {
+        try {
+            serviceLevelService.storagePoolPerformanceDatasetsQuery(serviceLevelId, interval);
             return success();
         } catch (DmeException e) {
             return failure(e.getMessage());
