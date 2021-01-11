aa48fb56a80b3eed3d025c5c1cafc4b12af2b746
wenyonglei <1226235821@qq.com>
2021-01-11 16:26:38 +0800

    vmfs详情页面修改

--- dmestore-ui/src/main/angular/src/app/routes/vmfs/volume-attribute/attribute.service.ts
+++ dmestore-ui/src/main/angular/src/app/routes/vmfs/volume-attribute/attribute.service.ts
@@ -22,4 +22,44 @@ export class VolumeInfo{
   provisionType: string;
   dudeplication: boolean;
   compression: boolean;
+  smartQos:SmartQos;
+}
+
+export class SmartQos {
+  /**
+   * name.
+   */
+  name:string;
+  /**
+   * 控制策略,0：保护IO下限，1：控制IO上限.
+   */
+  latency:number;
+  /**
+   * maxbandwidth.
+   */
+  maxbandwidth:number;
+  /**
+   * maxiops.
+   */
+  maxiops:number;
+  /**
+   * minbandwidth.
+   */
+  minbandwidth:number;
+  /**
+   * miniops.
+   */
+  miniops:number;
+  /**
+   * enabled.
+   */
+  enabled:boolean;
+  /**
+   * for update.0 下限 1 上限 2上限&下限
+   */
+  control_policy:string;
+  /**
+   * latencyUnit.
+   */
+  latencyUnit:string;
 }
