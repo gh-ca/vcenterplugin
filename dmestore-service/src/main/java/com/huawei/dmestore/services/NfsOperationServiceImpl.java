eddc80a36913334edf45cf87c2496ae008d9d5a1
lianq <qiang.lian@gh-ca.com>
2021-02-19 18:06:52 +0800

    调整逻辑端口与创建选择逻辑端口不一致问题

--- dmestore-service/src/main/java/com/huawei/dmestore/services/NfsOperationServiceImpl.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/services/NfsOperationServiceImpl.java
@@ -655,15 +655,12 @@ public class NfsOperationServiceImpl implements NfsOperationService {
         Map<String, String> respMap = new HashMap<>();
         String mgmt = "";
         String logicName = "";
-        List<LogicPorts> logicPort = dmeStorageService.getLogicPorts(storageId);
-        if (logicPort != null) {
-            JsonArray jsonArray = new JsonParser().parse(gson.toJson(logicPort)).getAsJsonArray();
-            for (JsonElement jsonElement : jsonArray) {
-                JsonObject element = jsonElement.getAsJsonObject();
-                String currentPortId1 = element.get("id").getAsString();
-                if (!StringUtils.isEmpty(currentPortId) && currentPortId1.equals(currentPortId)) {
-                    mgmt = ToolUtils.jsonToStr(element.get("mgmtIp"));
-                    logicName = ToolUtils.jsonToStr(element.get(NAME_FIELD));
+        List<LogicPorts> logicPorts = dmeStorageService.getLogicPorts(storageId);
+        if (logicPorts != null) {
+            for (LogicPorts logicPort : logicPorts) {
+                if (logicPort!=null && !StringUtils.isEmpty(currentPortId)&& logicPort.getId().equalsIgnoreCase(currentPortId)) {
+                    mgmt = logicPort.getMgmtIp();
+                    logicName = logicPort.getName();
                     break;
                 }
             }
