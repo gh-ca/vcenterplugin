7246021e48b9e8077580540dea075f0c10c9ebaf
wenyonglei <1226235821@qq.com>
2021-02-25 15:05:40 +0800

    nfs编辑、添加名称校验去掉“.”、“-”

--- dmestore-ui/src/main/angular/src/app/routes/nfs/nfs.component.ts
+++ dmestore-ui/src/main/angular/src/app/routes/nfs/nfs.component.ts
@@ -541,7 +541,7 @@ export class NfsComponent implements OnInit {
     if(this.addForm.nfsName==null) return false;
     if(this.oldNfsName==this.addForm.nfsName) return false;
     this.oldNfsName=this.addForm.nfsName;
-    let reg5:RegExp = new RegExp('^[0-9a-zA-Z-\u4e00-\u9fa5a"_""."]*$');
+    let reg5:RegExp = new RegExp('^[0-9a-zA-Z\u4e00-\u9fa5a"_"]*$');
     if(reg5.test(this.addForm.nfsName)){
       // 共享名称不能包含中文
       let reg5Two:RegExp = new RegExp('[\u4e00-\u9fa5]');
@@ -576,7 +576,7 @@ export class NfsComponent implements OnInit {
     if(this.addForm.shareName==null) return false;
     if(this.oldShareName==this.addForm.shareName) return false;
     this.oldShareName=this.addForm.shareName;
-    let reg5:RegExp = new RegExp('^[0-9a-zA-Z-\u4e00-\u9fa5a"_""."]*$');
+    let reg5:RegExp = new RegExp('^[0-9a-zA-Z\u4e00-\u9fa5a"_"]*$');
     if(reg5.test(this.addForm.shareName)){
       // 共享名称不能包含中文
       let reg5Two:RegExp = new RegExp('[\u4e00-\u9fa5]');
@@ -615,10 +615,10 @@ export class NfsComponent implements OnInit {
 
   checkFsName(){
     if(this.addForm.fsName==null) return false;
-    if(this.oldFsName=this.addForm.fsName) return false;
+    if(this.oldFsName==this.addForm.fsName) return false;
 
     this.oldFsName=this.addForm.fsName;
-    let reg5:RegExp = new RegExp('^[0-9a-zA-Z-\u4e00-\u9fa5a"_""."]*$');
+    let reg5:RegExp = new RegExp('^[0-9a-zA-Z\u4e00-\u9fa5a"_"]*$');
     if(reg5.test(this.addForm.fsName)){
       //验证重复
       this.matchErr=false;
