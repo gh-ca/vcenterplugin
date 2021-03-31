7314e2a8ff17bf7d8c3323c81946bb055ac05da3
wangxiangyong <111@163.com>
2021-03-31 12:27:07 +0800

    最佳实践根据exsi版本动态获取最佳实践检查、更新

--- dmestore-service/src/main/java/com/huawei/dmestore/services/bestpractice/DiskDiskMaxIOSizeImpl.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/services/bestpractice/DiskDiskMaxIOSizeImpl.java
@@ -3,8 +3,12 @@ package com.huawei.dmestore.services.bestpractice;
 import com.huawei.dmestore.utils.VCSDKUtils;
 import com.huawei.vmware.mo.HostMo;
 import com.huawei.vmware.util.VmwareContext;
+
 import com.vmware.vim25.AboutInfo;
 import com.vmware.vim25.ManagedObjectReference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * DiskDiskMaxIOSizeImpl
@@ -15,6 +19,11 @@ import com.vmware.vim25.ManagedObjectReference;
 public class DiskDiskMaxIOSizeImpl extends BaseBestPracticeService implements BestPracticeService {
     private static final long RECOMMEND_VALUE = 1024L;
 
+    private static final Map<String, Long> recommendMap = new HashMap() {{
+        put("6.7up", 32767L);
+        put("6.7low", 1024L);
+    }};
+
     @Override
     public String getHostSetting() {
         return "Disk.DiskMaxIOSize";
@@ -22,7 +31,7 @@ public class DiskDiskMaxIOSizeImpl extends BaseBestPracticeService implements Be
 
     @Override
     public Object getRecommendValue() {
-        return RECOMMEND_VALUE;
+        return "1024|32767";
     }
 
     @Override
@@ -47,34 +56,40 @@ public class DiskDiskMaxIOSizeImpl extends BaseBestPracticeService implements Be
 
     @Override
     public boolean check(VCSDKUtils vcsdkUtils, String objectId) throws Exception {
-        // ESXi版本检测，6.7U1以前的版本才建议进行修改
-        ManagedObjectReference mor = vcsdkUtils.getVcConnectionHelper().objectId2Mor(objectId);
-        VmwareContext context = vcsdkUtils.getVcConnectionHelper().getServerContext(objectId);
-        HostMo hostMo = this.getHostMoFactory().build(context, mor);
-        AboutInfo aboutInfo = hostMo.getHostAboutInfo();
-        String esxiApiVersion = aboutInfo.getApiVersion();
-        String bulidNum = aboutInfo.getBuild();
-        if(Float.valueOf(esxiApiVersion) > Float.valueOf("6.5")){
-            if(isSupportDefaultValue(bulidNum)){
-                return true;
-            }
-        }
-        return super.check(vcsdkUtils, objectId, getHostSetting(), getRecommendValue());
+        return super.check(vcsdkUtils, objectId, getHostSetting(),
+            recommendMap.get(getRecommendValueKey(vcsdkUtils, objectId)));
     }
 
-    private boolean isSupportDefaultValue(String bulidNum){
-        String[] isNotSupports = new String[]{"10176752","9484548","8941472","9214924","8169962"};
-        for(String str : isNotSupports){
-            if(str.equals(bulidNum)){
+    private boolean isSupportDefaultValue(String bulidNum) {
+        String[] isNotSupports = new String[] {"10176752", "9484548", "8941472", "9214924", "8169962"};
+        for (String str : isNotSupports) {
+            if (str.equals(bulidNum)) {
                 return false;
             }
         }
-
         return true;
     }
 
     @Override
     public void update(VCSDKUtils vcsdkUtils, String objectId) throws Exception {
-        super.update(vcsdkUtils, objectId, getHostSetting(), getRecommendValue());
+        super.update(vcsdkUtils, objectId, getHostSetting(),
+            recommendMap.get(getRecommendValueKey(vcsdkUtils, objectId)));
+    }
+
+    private String getRecommendValueKey(VCSDKUtils vcsdkUtils, String objectId) throws Exception {
+        // ESXi版本检测，6.7U1以前的版本才建议进行修改
+        ManagedObjectReference mor = vcsdkUtils.getVcConnectionHelper().objectId2Mor(objectId);
+        VmwareContext context = vcsdkUtils.getVcConnectionHelper().getServerContext(objectId);
+        HostMo hostMo = this.getHostMoFactory().build(context, mor);
+        AboutInfo aboutInfo = hostMo.getHostAboutInfo();
+        String esxiApiVersion = aboutInfo.getApiVersion().substring(0, 2);
+        String bulidNum = aboutInfo.getBuild();
+        String recommendValueKey = "6.7low";
+        if (Float.valueOf(esxiApiVersion) > Float.valueOf("6.5")) {
+            if (isSupportDefaultValue(bulidNum)) {
+                recommendValueKey = "6.7up";
+            }
+        }
+        return recommendValueKey;
     }
 }
