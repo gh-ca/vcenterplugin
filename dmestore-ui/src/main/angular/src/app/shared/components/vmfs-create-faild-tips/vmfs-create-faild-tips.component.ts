0c5c909895490d11719be890d88349186fc27625
xiangxinglong <xinglong.xiang@gh-ca.com>
2021-04-15 01:00:34 +0800

    vmfs 创建失败组件

---
+++ dmestore-ui/src/main/angular/src/app/shared/components/vmfs-create-faild-tips/vmfs-create-faild-tips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vmfs-create-faild-tips',
+  templateUrl: './vmfs-create-faild-tips.component.html',
+  styleUrls: ['./vmfs-create-faild-tips.component.scss']
+})
+export class VmfsCreateFaildTipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
