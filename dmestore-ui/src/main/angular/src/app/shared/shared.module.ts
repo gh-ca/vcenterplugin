d870de1b149499079945ad602235ff0c3603d417
xiangxinglong <xinglong.xiang@gh-ca.com>
2021-04-15 00:50:31 +0800

    vmfs 创建失败提示；vmfs 卸载显示主机 集群

--- dmestore-ui/src/main/angular/src/app/shared/shared.module.ts
+++ dmestore-ui/src/main/angular/src/app/shared/shared.module.ts
@@ -11,12 +11,13 @@ import { PageHeaderComponent } from './components/page-header/page-header.compon
 import { ErrorCodeComponent } from './components/error-code/error-code.component';
 /*  */
 import { BpPanelListMtuComponent } from './../routes/bestpractice/bp-panel-list-mtu/bp-panel-list-mtu.component';
+import { VmfsCreateFaildTipsComponent } from './components/vmfs-create-faild-tips/vmfs-create-faild-tips.component';
 
 const THIRD_MODULES = [
   MaterialModule,
   TranslateModule,
 ];
-const COMPONENTS = [BreadcrumbComponent, PageHeaderComponent, ErrorCodeComponent];
+const COMPONENTS = [BreadcrumbComponent, PageHeaderComponent, ErrorCodeComponent, VmfsCreateFaildTipsComponent];
 const COMPONENTS_DYNAMIC = [];
 const DIRECTIVES = [];
 const PIPES = [];
