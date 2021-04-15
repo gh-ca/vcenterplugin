6e37a1ce42dd54e333e266f1df8e9cd1737fecf3
xiangxinglong <xinglong.xiang@gh-ca.com>
2021-04-16 02:32:08 +0800

    0416
    1.vmfs nfs 状态 正常 告警 告示
    2.容量 v6 单位转换 KB MD GB TB PB

--- dmestore-ui/src/main/angular/src/app/app.helpers.ts
+++ dmestore-ui/src/main/angular/src/app/app.helpers.ts
@@ -39,3 +39,31 @@ export async function getURL(vm){
 export async function getQueryParams(vm){
   return new Promise(resovle => vm.route.queryParams.subscribe(resovle))
 }
+
+export const getColorByType = (prop, defaultValue = 'unset', type = 'a') => {
+  const color_map = {
+    a:{
+      1: 'green',
+      2: 'gold',
+      3: 'red'
+    }
+  }
+  const _colorMap = color_map[type];
+  let target = _colorMap[prop];
+  target = target ? target:defaultValue;
+  return target;
+}
+
+export const getLabelByValue = (value, type, defaultValue = "") => {
+  const LABEL_MAP = {
+    alarm_state_map : {
+      1: 'enum.status.normal',
+      2: 'enum.status.warning',
+      3: 'enum.status.alert',
+    }
+  }
+  const _labelMap = LABEL_MAP[type];
+  let target = _labelMap[value];
+  target = target ? target:defaultValue;
+  return target;
+}
