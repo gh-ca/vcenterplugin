29cd8bedac1cbe00f105ec09e38bb1ab0aa2a2d0
xiangxinglong <xinglong.xiang@gh-ca.com>
2021-04-16 19:01:50 +0800

    最佳实践 期望值多行展示

--- dmestore-ui/src/main/angular/src/app/routes/bestpractice/bp-panel-list-mtu/bp-panel-list-mtu.component.ts
+++ dmestore-ui/src/main/angular/src/app/routes/bestpractice/bp-panel-list-mtu/bp-panel-list-mtu.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy, Input, SimpleChange, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy,
+  Input,
+  SimpleChange,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { CommonService } from './../../common.service';
 import { Host } from '../bestpractice.component';
 import { HttpClient } from '@angular/common/http';
 import { handleRes, stringParseObj, valueOrDefault } from './../../../app.helpers';
 import { isMockData, mockData } from 'mock/mock';
-
 
 /*  */
 const URL_GET_VIRTUAL_NIC_LIST = 'v1/bestpractice/virtual-nic';
@@ -16,10 +24,8 @@ const URL_UPDATE_VIRTUAL_NIC_LIST = 'v1/bestpractice/virtual-nic/update';
   templateUrl: './bp-panel-list-mtu.component.html',
   styleUrls: ['./bp-panel-list-mtu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [TranslatePipe]
+  providers: [TranslatePipe],
 })
-
-
 export class BpPanelListMtuComponent implements OnInit {
   @Input() hostList: Host[];
   @Input() currentItem;
@@ -33,50 +39,45 @@ export class BpPanelListMtuComponent implements OnInit {
   hostSelected = [];
   hostListChange;
 
-
   constructor(
     private http: HttpClient,
     private $utils: CommonService,
     private translatePipe: TranslatePipe,
-    private cdr: ChangeDetectorRef,
+    private cdr: ChangeDetectorRef
   ) {
     this.onApply = new EventEmitter();
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   resetVariables() {
     this.hostSelected = [];
     this.hostTotal = 0;
   }
 
-
   async getVirtualNicList(params) {
     return new Promise((resolve, reject) => {
-      this.http.post(URL_GET_VIRTUAL_NIC_LIST, params).subscribe(resolve, reject)
-    })
+      this.http.post(URL_GET_VIRTUAL_NIC_LIST, params).subscribe(resolve, reject);
+    });
   }
   async applyBP(params) {
     return new Promise((resolve, reject) => {
-      this.http.post(URL_UPDATE_VIRTUAL_NIC_LIST, params).subscribe(resolve, reject)
-    })
+      this.http.post(URL_UPDATE_VIRTUAL_NIC_LIST, params).subscribe(resolve, reject);
+    });
   }
 
-
-  async handleCurrentItemChange() {
-
-  }
+  async handleCurrentItemChange() {}
 
   async handleHostListChange() {
-    const isHostListChange = this.hostListChange && Array.isArray(this.hostListChange.currentValue)
+    const isHostListChange = this.hostListChange && Array.isArray(this.hostListChange.currentValue);
     if (isHostListChange) {
       const _hostList = this.hostListChange.currentValue;
-      this.resetVariables()
+      this.resetVariables();
       if (_hostList.length > 0) {
         const targetObjectIds = this.hostListChange?.currentValue?.map(i => i.hostObjectId);
         let res;
         if (isMockData) {
-          res = mockData.BESTPRACTICE_VIRTUAL_NIC
+          res = mockData.BESTPRACTICE_VIRTUAL_NIC;
         } else {
           this.hostIsLoading = true;
           try {
@@ -97,20 +98,20 @@ export class BpPanelListMtuComponent implements OnInit {
                 device: valueOrDefault(i.device),
                 nicIp: valueOrDefault(i.nicIp),
                 mtu: valueOrDefault(i.mtu),
-              }
+              };
             });
             this.hostTotal = this.dataTableList.length;
             this.cdr.detectChanges();
-          }
-        })
+          },
+        });
       } else {
         this.dataTableList = [];
       }
       this.cdr.detectChanges();
     }
-
   }
 
+  /* 监听prop的变动 */
   ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
     this.hostListChange = changes['hostList'];
     /* handle hostList changed */
@@ -129,18 +130,18 @@ export class BpPanelListMtuComponent implements OnInit {
     console.log(this.hostSelected);
 
     const paramArray = this.hostSelected.map(i => ({
-      "device": i.device,
-      "hostObjectId": i.hostObjId
-    }))
+      device: i.device,
+      hostObjectId: i.hostObjId,
+    }));
     try {
       const res = await this.applyBP(paramArray);
       this.onApply.emit(res);
       /* refresh */
       this.handleHostListChange();
-      console.log("handleHostListChange", res);
+      console.log('handleHostListChange', res);
     } catch (error) {
       this.onApply.emit(error);
-      console.log("实施失败", error);
+      console.log('实施失败', error);
     }
   }
 
