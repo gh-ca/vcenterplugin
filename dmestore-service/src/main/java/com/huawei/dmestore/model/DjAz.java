f716c2c6ff291417e0d6f9c809be20361ac6b109
chenjie <jie.chen@gh-ca.com>
2021-04-16 16:44:00 +0800

    给存储设备设置AZ信息

---
+++ dmestore-service/src/main/java/com/huawei/dmestore/model/DjAz.java
@@ -0,0 +1,45 @@
+package com.huawei.dmestore.model;
+
+public class DjAz {
+
+    private String id;
+
+    /**
+     * name
+     */
+    private String name;
+    private long lastModified;
+    private long createTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(long lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
+}
