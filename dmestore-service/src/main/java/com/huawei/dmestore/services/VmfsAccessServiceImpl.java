3fc02bbd94854912de954537f0055cadc6afb5bd
chaoyao-gh-ca <chao.yao@gh-ca.com>
2021-04-19 14:33:02 +0800

    修改VMFS的编辑页面的查询接口，不满足原业务需求

--- dmestore-service/src/main/java/com/huawei/dmestore/services/VmfsAccessServiceImpl.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/services/VmfsAccessServiceImpl.java
@@ -981,8 +981,8 @@ public class VmfsAccessServiceImpl implements VmfsAccessService {
         if (responseEntity.getStatusCodeValue() == HttpStatus.OK.value()) {
             String body = responseEntity.getBody();
             JsonObject jsonObject = new JsonParser().parse(body).getAsJsonObject();
-            JsonArray jsonArray = jsonObject.get("result_list").getAsJsonArray();
-            //JsonArray jsonArray = jsonObject.get("results").getAsJsonArray();
+            //JsonArray jsonArray = jsonObject.get("result_list").getAsJsonArray();
+            JsonArray jsonArray = jsonObject.get("results").getAsJsonArray();
             for (JsonElement jsonElement : jsonArray) {
                 JsonObject element = jsonElement.getAsJsonObject();
                 String id = ToolUtils.jsonToStr(element.get("host_id"));
@@ -990,11 +990,11 @@ public class VmfsAccessServiceImpl implements VmfsAccessService {
                 String resultMessage = ToolUtils.jsonToStr(element.get("result_message"));
                 // 连通性异常主机结果统计
                 Map<String, String> result = new HashMap<>();
-                //if (status.equalsIgnoreCase("NOT_CONNECT")) {
-                if (status.equalsIgnoreCase("FAILED")) {
+                if (status.equalsIgnoreCase("NOT_CONNECT")) {
+                //if (status.equalsIgnoreCase("FAILED")) {
                     String hostName = getDmeHostNameById(id);
-                    result.put(hostName, resultMessage);
-                    //result.put(hostName, "启动器处于离线状态！");
+                    //result.put(hostName, resultMessage);
+                    result.put(hostName, "启动器处于离线状态！");
                 }
                 if (result.size() != 0) {
                     results.add(result);
