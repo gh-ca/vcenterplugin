cf7d522bd0414336bfbcccf1f357b499a18b346b
xiaohuan.liu <xiaohuan.liu@gh-ca.com>
2021-05-27 15:39:55 +0800

    modify vmware performance interface

--- dmestore-service/src/main/java/com/huawei/dmestore/services/DmePerformanceService.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/services/DmePerformanceService.java
@@ -6,11 +6,25 @@ import com.huawei.dmestore.exception.DmeException;
 
 import java.util.Map;
 
+/**
+ * vmware性能接口
+ */
 public interface DmePerformanceService {
-
+    /**
+     * 获取存储的使用情况 countId 268，disk.used.latest	KB
+     * @param storageId
+     * @return
+     * @throws DmeException
+     */
     String getPerformanceDataStore(String storageId) throws DmeException;
 
     Map<String, Object> getPerformance(Map<String, Object> params) throws  DmeException;
 
+    /**
+     * 获取vmware指定对象指定指标的当前性能数据
+     * @param params obj_ids，indicator_ids
+     * @return
+     * @throws DmeException
+     */
     Map<String, Object> getPerformanceCurrent(Map<String, Object> params) throws DmeException;
 }
--- dmestore-service/src/main/java/com/huawei/dmestore/utils/VCSDKUtils.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/utils/VCSDKUtils.java
@@ -3400,7 +3400,7 @@ public class VCSDKUtils {
             qSpec.setFormat("csv");
             qSpec.setIntervalId(new Integer(300));
             PerfMetricId perfMetricId = new PerfMetricId();
-            perfMetricId.setCounterId(268);
+            perfMetricId.setCounterId(268);//disk.used.latest KB
             perfMetricId.setInstance("");
             qSpec.getMetricId().add(perfMetricId);
             List<PerfQuerySpec> perfQuerySpecs = new ArrayList<>();
