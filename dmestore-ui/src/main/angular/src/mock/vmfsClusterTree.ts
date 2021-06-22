702df3e2a5d201e9268fb679d6a51cf2fa6b16ff
Wei Chaoqian <chaoqian.wei@gh-ca.com>
2021-06-22 21:33:29 +0800

    6/22 21:32 添加vmfs删除警示信息 修改vmfs挂载bug，接口返回不过滤，增加是否可选字段，来判断渲染时是否可选；提交时当集群下的children的新增字段为false时，提交方式不能为集群 修改创建vmfs存储时部分成功提示

--- dmestore-ui/src/main/angular/src/mock/vmfsClusterTree.ts
+++ dmestore-ui/src/main/angular/src/mock/vmfsClusterTree.ts
@@ -4,23 +4,27 @@ export const vmfsClusterTreeData = [
     clusterName: 'baa',
     deviceType:'cluster',
     children: [],
+    flag:true
   },
   {
     clusterId: 'urn:vmomi:ClusterComputeResource:domain-c2063:674908e5-ab21-4079-9cb1-596358ee5dd1',
     clusterName: 'ghca_cluster02',
     deviceType:'cluster',
+    flag:true,
     children: [
       {
         clusterId: 'urn:vmomi:HostSystem:host-2055:674908e5-ab21-4079-9cb1-596358ee5dd1',
         clusterName: '10.143.133.197',
         deviceType:'host',
         children: null,
+        flag:true
       },
       {
         clusterId: 'urn:vmomi:HostSystem:host-2056:674908e5-ab21-4079-9cb1-596358ee5dd1',
         clusterName: '10.143.133.197',
         deviceType:'host',
         children: null,
+        flag:false
       },
     ],
   },
@@ -28,12 +32,14 @@ export const vmfsClusterTreeData = [
     clusterId: 'urn:vmomi:ClusterComputeResource:domain-c2044:674908e5-ab21-4079-9cb1-596358ee5dd1',
     clusterName: 'lq_datacenter',
     deviceType:'cluster',
+    flag: true,
     children: [
       {
         clusterId: 'urn:vmomi:HostSystem:host-16256:674908e5-ab21-4079-9cb1-596358ee5dd1',
         clusterName: '10.143.133.196',
         deviceType:'host',
         children: null,
+        flag: false,
       },
     ],
   },
@@ -41,12 +47,21 @@ export const vmfsClusterTreeData = [
     clusterId: 'urn:vmomi:ClusterComputeResource:domain-c2038:674908e5-ab21-4079-9cb1-596358ee5dd1',
     clusterName: 'ghca_cluseter01',
     deviceType:'cluster',
+    flag: true,
     children: [
       {
         clusterId: 'urn:vmomi:HostSystem:host-1034:674908e5-ab21-4079-9cb1-596358ee5dd1',
         clusterName: '10.143.133.17',
         deviceType:'host',
         children: null,
+        flag: true,
+      },
+      {
+        clusterId: 'urn:vmomi:HostSystem:host-1034:674908e5-ab21-4079-9cb1-596358ee5dd1',
+        clusterName: '10.143.133.17',
+        deviceType:'host',
+        children: null,
+        flag: true,
       },
     ],
   },
