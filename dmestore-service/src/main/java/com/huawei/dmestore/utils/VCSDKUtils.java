43af7d5ded1817dedd99a722f04dd6b3feb169c8
chaoyao-gh-ca <chao.yao@gh-ca.com>
2021-07-20 17:56:00 +0800

    问题单问题修改

--- dmestore-service/src/main/java/com/huawei/dmestore/utils/VCSDKUtils.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/utils/VCSDKUtils.java
@@ -2184,7 +2184,6 @@ public class VCSDKUtils {
                             // 只挂载其它的主机
                             if (host1 != null && !objHostName.equals(host1.getName())) {
                                 mountVmfsNew02(objDataStoreName, dataStoreObjectId, host1);
-                                hostRescanVmfs(host1.getName());
                             }
                         } catch (Exception e) {
                             logger.error("mount Vmfs On Cluster error:{}", e.getMessage());
@@ -2200,7 +2199,6 @@ public class VCSDKUtils {
                     // 只挂载其它的主机
                     if (hostmo != null && !objHostName.equals(hostmo.getName())) {
                         mountVmfsNew02(objDataStoreName, dataStoreObjectId, hostmo);
-                        hostRescanVmfs(hostmo.getName());
                     }
                 } catch (Exception e) {
                     logger.error("mount Vmfs On Cluster error:{}", e.getMessage());
@@ -2424,6 +2422,7 @@ public class VCSDKUtils {
 
         // 挂载前重新扫描datastore
         logger.info("refreshStorageSystem before mount Vmfs!");
+        hostMo.getHostStorageSystemMo().rescanVmfs();
         hostMo.getHostStorageSystemMo().refreshStorageSystem();
         try {
             TimeUnit.SECONDS.sleep(2);
