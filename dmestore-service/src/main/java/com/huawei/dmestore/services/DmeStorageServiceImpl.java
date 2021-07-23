2741dbbe9d2ecbc60807dfe3256ffb40ea72b246
chaoyao-gh-ca <chao.yao@gh-ca.com>
2021-07-23 14:52:56 +0800

    问题单修改：增加日志，v6设备磁盘类型显示LUN

--- dmestore-service/src/main/java/com/huawei/dmestore/services/DmeStorageServiceImpl.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/services/DmeStorageServiceImpl.java
@@ -1379,19 +1379,29 @@ public class DmeStorageServiceImpl implements DmeStorageService {
     private String getDiskType(JsonObject poolObject) throws DmeException {
         String diskType = "";
         if (!StringUtils.isEmpty(ToolUtils.jsonToStr(poolObject.get("tier0DiskType"))) && !"0".equalsIgnoreCase(ToolUtils.jsonToStr(poolObject.get("tier0DiskType")))){
-            diskType = DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier0DiskType")));
+            if (!StringUtils.isEmpty(DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier0DiskType"))))) {
+                diskType = DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier0DiskType")));
+            }
         }
         if (!StringUtils.isEmpty(ToolUtils.jsonToStr(poolObject.get("tier1DiskType"))) && !"0".equalsIgnoreCase(ToolUtils.jsonToStr(poolObject.get("tier1DiskType")))){
-            diskType = diskType + "/" + DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier1DiskType")));
+            if (!StringUtils.isEmpty(DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier1DiskType"))))) {
+                diskType = diskType + "/" + DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier1DiskType")));
+            }
         }
         if (!StringUtils.isEmpty(ToolUtils.jsonToStr(poolObject.get("tier2DiskType"))) && !"0".equalsIgnoreCase(ToolUtils.jsonToStr(poolObject.get("tier2DiskType")))){
-            diskType = diskType + "/" +DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier2DiskType")));
+            if (!StringUtils.isEmpty(DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier2DiskType"))))) {
+                diskType = diskType + "/" + DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier2DiskType")));
+            }
         }
         if (!StringUtils.isEmpty(ToolUtils.jsonToStr(poolObject.get("tier3DiskType"))) && !"0".equalsIgnoreCase(ToolUtils.jsonToStr(poolObject.get("tier3DiskType")))){
-            diskType = diskType + "/" + DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier3DiskType")));
+            if (!StringUtils.isEmpty(DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier3DiskType"))))) {
+                diskType = diskType + "/" + DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier3DiskType")));
+            }
         }
         if (!StringUtils.isEmpty(ToolUtils.jsonToStr(poolObject.get("tier4DiskType"))) && !"0".equalsIgnoreCase(ToolUtils.jsonToStr(poolObject.get("tier4DiskType")))){
-            diskType = diskType + "/" +DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier4DiskType")));
+            if (!StringUtils.isEmpty(DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier4DiskType"))))) {
+                diskType = diskType + "/" + DmeConstants.TIER_DISK_TYPE.get(ToolUtils.jsonToStr(poolObject.get("tier4DiskType")));
+            }
         }
         if (diskType.startsWith("/")){
             diskType = diskType.substring(1);
