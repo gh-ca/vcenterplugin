5469b31e1d66f168d18fa1ea3f266a6635c458a7
chenjie <jie.chen@gh-ca.com>
2021-07-28 15:31:29 +0800

    服务等级性能接口修改

--- dmestore-service/src/main/java/com/huawei/dmestore/services/ServiceLevelServiceImpl.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/services/ServiceLevelServiceImpl.java
@@ -911,11 +911,11 @@ public class ServiceLevelServiceImpl implements ServiceLevelService {
                 JsonArray pers = i.getAsJsonObject("tier-pool-sum-totalCapacity-perform").getAsJsonArray("buckets");
                 if (pers != null && pers.size() > 0) {
                     JsonObject per = pers.get(0).getAsJsonObject();
-                    dmeDatasetBean.setTotalCapacity(per.getAsJsonObject("sum-totalCapacity").get("value").getAsFloat());
-                    dmeDatasetBean.setUsedCapacity(per.getAsJsonObject("sum-usedCapacity").get("value").getAsFloat());
-                    dmeDatasetBean.setThroughput(per.getAsJsonObject("sum-throughput").get("value").getAsFloat());
-                    dmeDatasetBean.setResponseTime(per.getAsJsonObject("max-responseTime").get("value").getAsFloat());
-                    dmeDatasetBean.setBandwidth(per.getAsJsonObject("max-bandwidth").get("value").getAsFloat());
+                    dmeDatasetBean.setTotalCapacity(getFloatValue(per.getAsJsonObject("sum-totalCapacity").get("value")));
+                    dmeDatasetBean.setUsedCapacity(getFloatValue(per.getAsJsonObject("sum-usedCapacity").get("value")));
+                    dmeDatasetBean.setThroughput(getFloatValue(per.getAsJsonObject("sum-throughput").get("value")));
+                    dmeDatasetBean.setResponseTime(getFloatValue(per.getAsJsonObject("max-responseTime").get("value")));
+                    dmeDatasetBean.setBandwidth(getFloatValue(per.getAsJsonObject("max-bandwidth").get("value")));
                     dmeDatasetBean.setTierNativeId(per.get("key").getAsString());
 
                     dmeDatasetBeans.add(dmeDatasetBean);
@@ -927,6 +927,14 @@ public class ServiceLevelServiceImpl implements ServiceLevelService {
         DmeDatasetsQueryResponse dmeDatasetsQueryResponse = new DmeDatasetsQueryResponse();
         dmeDatasetsQueryResponse.setDatas(dmeDatasetBeans);
         return gson.toJson(dmeDatasetsQueryResponse);
+    }
+
+    private float getFloatValue(JsonElement element){
+        if (element instanceof JsonNull){
+            return 0.0f;
+        } else{
+            return element.getAsFloat();
+        }
     }
 
 
