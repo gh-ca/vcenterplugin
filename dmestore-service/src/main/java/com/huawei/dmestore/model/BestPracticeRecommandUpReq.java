40762db8eca7ce939124e2c5bea988cb2e721e3b
xiangyong.wang <xiangyong.wang@gh-ca.com>
2021-09-13 10:47:56 +0800

    vmfs自动扩容最佳实践，实现代码提交

---
+++ dmestore-service/src/main/java/com/huawei/dmestore/model/BestPracticeRecommandUpReq.java
@@ -0,0 +1,23 @@
+package com.huawei.dmestore.model;
+
+public class BestPracticeRecommandUpReq {
+    private String recommandValue;
+
+    private String repairAction;
+
+    public String getRecommandValue() {
+        return recommandValue;
+    }
+
+    public void setRecommandValue(String recommandValue) {
+        this.recommandValue = recommandValue;
+    }
+
+    public String getRepairAction() {
+        return repairAction;
+    }
+
+    public void setRepairAction(String repairAction) {
+        this.repairAction = repairAction;
+    }
+}
