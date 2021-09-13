d4eb06f637241555509c486ea97116de94257193
chaoqian.wei@gh-ca.com <chaoqian.wei@gh-ca.com>
2021-09-13 10:09:29 +0800

    9/13 提交错误

--- dmestore-service/src/main/java/com/huawei/dmestore/filter/CorsFilter.java
+++ dmestore-service/src/main/java/com/huawei/dmestore/filter/CorsFilter.java
@@ -27,11 +27,11 @@ public class CorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-        throws IOException, ServletException {
+            throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
         res.addHeader("Access-Control-Allow-Credentials", "true");
-        res.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-//        res.addHeader("Access-Control-Allow-Origin", "*");
+        //res.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.addHeader("Access-Control-Allow-Origin", "*");
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN");
         if ("OPTIONS".equals(((HttpServletRequest) request).getMethod())) {
