32654902ae31af8a8180fdf0c0e0da9e2d5e187d
chaoyao-gh-ca <81681852+chaoyao-gh-ca@users.noreply.github.com>
2021-10-25 15:36:27 +0800

    卸载逻辑修改

--- dmestore-ui/src/main/angular/src/app/routes/vmfs/mount/mount.component.ts
+++ dmestore-ui/src/main/angular/src/app/routes/vmfs/mount/mount.component.ts
@@ -686,7 +686,8 @@ export class MountComponent implements OnInit {
         this.unmountForm.dataStoreObjectIds = unmountObjIds;
         this.modalHandleLoading = true;
         console.log(this.unmountForm)
-        this.remoteSrv.unmountVMFS(_.merge(this.unmountForm,{ clusterIds: [this.unmountForm.clusterId]},{language:this.language})).subscribe((result: any) => {
+        //this.remoteSrv.unmountVMFS(_.merge(this.unmountForm,{ clusterIds: [this.unmountForm.clusterId]},{language:this.language})).subscribe((result: any) => {
+        this.remoteSrv.unmountVMFS(_.merge(this.unmountForm,{language:this.language})).subscribe((result: any) => {
           this.modalHandleLoading = false;
           if (result.code === '200') {
             // console.log('unmount ' + this.rowSelected[0].name + ' success');
@@ -984,7 +985,8 @@ export class MountComponent implements OnInit {
       };
       console.log('chooseDevice',this.chooseDevice)
       console.log('unmountForm',this.unmountForm)
-      let queryUnmount={}
+      const params = _.merge(this.unmountForm,{ hostIds: this.chooseDevice.map(i=>i.deviceId) },{language:this.language});
+      /*let queryUnmount={}
       queryUnmount=this.unmountForm
       let clusterIds = this.chooseDevice.filter(i => i.deviceType === 'cluster')
       let hostIds = this.chooseDevice.filter(i => i.deviceType === 'host')
@@ -993,13 +995,15 @@ export class MountComponent implements OnInit {
       }
       if (hostIds.length>0){
         queryUnmount=_.merge(queryUnmount,{hostIds:hostIds.map(i=>i.deviceId)}, {language: this.language})
-      }
+      }*/
 
 
       // const res = await this.commonService.remoteVmfs_Unmount(params);
       // handlerUnmountVmfsSuccess(res);
-      console.log(queryUnmount)
-      this.remoteSrv.unmountVMFS(queryUnmount).subscribe(handlerUnmountVmfsSuccess);
+      console.log(params)
+      this.remoteSrv.unmountVMFS(params).subscribe(handlerUnmountVmfsSuccess);
+      /*console.log(queryUnmount)
+      this.remoteSrv.unmountVMFS(queryUnmount).subscribe(handlerUnmountVmfsSuccess);*/
     }
     if(this.chooseDevice.length>0){
       submit()
