f34243c9678d0b388a484df3c225efe90b87dbca
chaoqian.wei@gh-ca.com <chaoqian.wei@gh-ca.com>
2021-12-13 18:18:43 +0800

    12/13 问题单：vmfs创建容量上限，基于v5创建nfs共享名称提示添加半角，基于v5创建nfs提示信息统一

--- dmestore-ui/src/main/angular/src/app/routes/nfs/subpages/reduce/nfs-reduce.component.ts
+++ dmestore-ui/src/main/angular/src/app/routes/nfs/subpages/reduce/nfs-reduce.component.ts
@@ -3,59 +3,57 @@ import {GlobalsService} from "../../../../shared/globals.service";
 import {NfsReduceService} from "./nfs-reduce.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {UpdateNfs} from "../../nfs.service";
-
 @Component({
   selector: 'app-reduce',
   templateUrl: './nfs-reduce.component.html',
   styleUrls: ['./nfs-reduce.component.scss'],
   providers: [NfsReduceService]
 })
-export class NfsReduceComponent implements OnInit {
-  storeObjectId: string;
+export class NfsReduceComponent implements OnInit{
+  storeObjectId:string;
   viewPage: string;
-  unit = 'GB';
-  newCapacity = 1;
+  unit='GB';
+  newCapacity = 0;
   pluginFlag: string;//来至插件的标记
   rowSelected = []; // 当前选中数据
   errorMsg: string;
   modalLoading = false; // 数据加载loading
   modalHandleLoading = false; // 数据处理loading
-  fsId: string;
+  fsId:string;
   reduceSuccessShow = false;// 缩容成功窗口
-  capacityErr = true;
-  updateNfs = new UpdateNfs();
+  capacityErr= true;
+  updateNfs=new UpdateNfs();
 
   minCapacity;
-  maxOpCapacity = '3.211';
-  reduceErrorShow = false
-  newCapacityError = false
+  maxOpCapacity;
+  reduceErrorShow=false
+  newCapacityError=false
 
   constructor(private reduceService: NfsReduceService, private gs: GlobalsService,
-              private activatedRoute: ActivatedRoute, private router: Router, private cdr: ChangeDetectorRef) {
+              private activatedRoute: ActivatedRoute,private router:Router, private cdr: ChangeDetectorRef){
   }
-
   ngOnInit(): void {
     //入口是DataSource
-    this.viewPage = 'reduce_plugin'
+    this.viewPage='reduce_plugin'
     this.activatedRoute.queryParams.subscribe(queryParam => {
       this.fsId = queryParam.fsId;
-      this.pluginFlag = queryParam.flag;
-      this.storeObjectId = queryParam.objectId;
+      this.pluginFlag =queryParam.flag;
+      this.storeObjectId =queryParam.objectId;
     });
-    if (this.pluginFlag == null) {
+    if(this.pluginFlag==null){
       //入口来至Vcenter
       const ctx = this.gs.getClientSdk().app.getContextObjects();
-      if (ctx != null) {
-        this.storeObjectId = ctx[0].id;
+      if(ctx!=null){
+        this.storeObjectId=ctx[0].id;
       }
       // this.storeObjectId="urn:vmomi:Datastore:datastore-4072:674908e5-ab21-4079-9cb1-596358ee5dd1";
       console.log("this.storeObjectId", this.storeObjectId);
-      console.log("ctx!=null", ctx != null);
-      this.viewPage = 'reduce_vcenter'
+      console.log("ctx!=null", ctx!=null);
+      this.viewPage='reduce_vcenter'
       this.modalLoading = true;
       this.reduceService.getStorageById(this.storeObjectId).subscribe((result: any) => {
         this.modalLoading = false;
-        if (result.code === '200') {
+        if (result.code === '200'){
           this.fsId = result.data.fsId;
         }
         this.cdr.detectChanges();
@@ -64,35 +62,33 @@ export class NfsReduceComponent implements OnInit {
     this.modalLoading = true;
     this.reduceService.getNfsDetailById(this.storeObjectId).subscribe((result: any) => {
       this.modalLoading = false;
-      if (result.code === '200') {
+      if (result.code === '200'){
         this.updateNfs = result.data;
-        const capacity = this.updateNfs.capacity;
-        if (capacity > 3) {
-          this.maxOpCapacity = (capacity - 3).toFixed(3)
-        } else {
-          this.reduceErrorShow = true
+        const capacity =  this.updateNfs.capacity;
+        if (capacity>3){
+          this.maxOpCapacity=(capacity-3).toFixed(3)
+        }else {
+          this.reduceErrorShow=true
         }
       }
       this.cdr.detectChanges();
     });
+    this.modalLoading=false
   }
-
-  backToNfsList() {
-    this.modalLoading = false;
-    this.errorMsg = null;
+  backToNfsList(){
+    this.modalLoading=false;
+    this.errorMsg=null;
     this.router.navigate(['nfs']);
   }
-
-  closeModel() {
-    this.errorMsg = null;
-    this.modalLoading = false;
+  closeModel(){
+    this.errorMsg=null;
+    this.modalLoading=false;
     this.gs.getClientSdk().modal.close();
   }
-
-  reduceCommit() {
-    let v = this.checkNewCapacity();
-    if (!v) return;
-    this.modalHandleLoading = true;
+  reduceCommit(){
+    let v=this.checkNewCapacity();
+    if(!v) return;
+    this.modalHandleLoading=true;
     let capacity;
     switch (this.unit) {
       case 'TB':
@@ -109,45 +105,43 @@ export class NfsReduceComponent implements OnInit {
         break;
     }
     var params;
-    if (this.pluginFlag == 'plugin') {
-      params = {
+    if (this.pluginFlag=='plugin'){
+      params={
         "fileSystemId": this.fsId,
         "storeObjectId": this.storeObjectId,
-        "expand": false,
+        "expand":false,
         "capacity": capacity
       }
     }
-    if (this.pluginFlag == null) {
-      params = {
+    if(this.pluginFlag==null){
+      params={
         "storeObjectId": this.storeObjectId,
-        "expand": false,
+        "expand":false,
         "fileSystemId": this.fsId,
         "capacity": capacity
       }
     }
     this.reduceService.changeCapacity(params).subscribe((result: any) => {
-      this.modalHandleLoading = false;
-      if (result.code === '200') {
+      this.modalHandleLoading=false;
+      if (result.code === '200'){
         this.reduceSuccessShow = true;
-      } else {
+      }else{
         this.errorMsg = '1';
-        console.log("Reduce failed:", result.description);
+        console.log("Reduce failed:",result.description);
       }
       this.cdr.detectChanges();
     });
   }
-
   /**
    * 确认操作结果并关闭窗口
    */
   confirmActResult() {
-    if (this.pluginFlag == 'plugin') {
+    if(this.pluginFlag=='plugin'){
       this.backToNfsList();
-    } else {
+    }else{
       this.closeModel();
     }
   }
-
   /* checkCapacity(){
      let capacity;
      switch (this.unit) {
@@ -167,10 +161,9 @@ export class NfsReduceComponent implements OnInit {
        this.capacityErr=true;
        return true;
      }
-
    }*/
-  checkNewCapacity() {
-    console.log('reduce', this.newCapacity)
+  checkNewCapacity(){
+    console.log('reduce',this.newCapacity)
     console.log(typeof this.newCapacity)
     let capacity
     switch (this.unit) {
@@ -181,12 +174,12 @@ export class NfsReduceComponent implements OnInit {
         capacity = this.newCapacity;
         break;
     }
-    if (capacity > parseFloat(this.maxOpCapacity) || capacity < 0.001) {
-      this.newCapacity = 1
-      this.newCapacityError = true
+    if (capacity>parseFloat(this.maxOpCapacity)||capacity<0.001){
+      this.newCapacity=null
+      this.newCapacityError=true
       return false
-    } else {
-      this.newCapacityError = false
+    }else {
+      this.newCapacityError=false
       return true
     }
   }
